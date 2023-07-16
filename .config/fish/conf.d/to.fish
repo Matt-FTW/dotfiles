4661ad04b5bf3cba63e72801ee2d3c3848973a4f
Sergio Laín <sergiolain11@gmail.com>
2023-07-16 23:39:36 +0200

    new cli tools aliases for fish

--- .config/fish/conf.d/to.fish
+++
@@ -1,3 +0,0 @@
-if test -z "$TO_DIR"
-  set -U TO_DIR ~/.tofish
-end
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     neofetch
+    zoxide init fish | source
 end
 
 alias code='vscodium'
@@ -11,15 +12,18 @@ alias matrix='unimatrix'
 alias q='exit'
 alias install='sudo pacman -S '
 alias uninstall='sudo pacman -R '
-alias yayinstall='yay -S '
-alias yayuninstall='yay -R '
+alias yinstall='yay -S '
+alias yuninstall='yay -R '
 alias update='sudo pacman -Syu'
-alias yayupdate='yay -Syu'
+alias yupdate='yay -Syu'
 alias search='pacseek'
 alias gpt='tgpt'
 alias clock='tty-clock -sbc'
-alias l='xplr'
+alias files='xplr'
 alias sysproc='sysz'
 alias cat='bat'
 alias dots='yadm enter lazygit'
 alias audiorestart='systemctl --user restart wireplumber pipewire pipewire-pulse'
+alias info='tldr'
+alias cd='z'
+alias disks='duf'
