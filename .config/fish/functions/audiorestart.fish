5a204a3020ec7f48e1894fc08e293a626e0b279a
Sergio Laín <sergiolain11@gmail.com>
2023-09-05 12:27:26 +0200

    New script for restarting audio, new binds for hyprland documented and delition of unused nvim plugins

--- .config/fish/functions/audiorestart.fish
+++
@@ -1,4 +0,0 @@
-function audiorestart --wraps='systemctl --user restart wireplumber pipewire pipewire-pulse' --description 'alias audiorestart=systemctl --user restart wireplumber pipewire pipewire-pulse'
-  systemctl --user restart wireplumber pipewire pipewire-pulse $argv
-        
-end
