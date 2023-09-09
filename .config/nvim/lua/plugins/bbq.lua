1cf1b693805e1c4e47dc8d29872ee8feaf21a450
Sergio Laín <sergiolain11@gmail.com>
2023-09-09 23:17:40 +0200

    Barbacue plugin for neovim

---
+++ .config/nvim/lua/plugins/bbq.lua
@@ -0,0 +1,14 @@
+return {
+    {
+        "utilyre/barbecue.nvim",
+        name = "barbecue",
+        version = "*",
+        dependencies = {
+            "SmiteshP/nvim-navic",
+            "nvim-tree/nvim-web-devicons", -- optional dependency
+        },
+        opts = {
+            -- configurations go here
+        },
+    },
+}
---
+++ .config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,79 @@
+return {
+    {
+        "nvim-lualine/lualine.nvim",
+        event = "VeryLazy",
+        opts = function()
+            local icons = require("lazyvim.config").icons
+            local Util = require("lazyvim.util")
+
+            return {
+                options = {
+                    theme = "auto",
+                    globalstatus = true,
+                    disabled_filetypes = { statusline = { "dashboard", "alpha" } },
+                },
+                sections = {
+                    lualine_a = { "mode" },
+                    lualine_b = { "branch" },
+                    lualine_c = {
+                        {
+                            "diagnostics",
+                            symbols = {
+                                error = icons.diagnostics.Error,
+                                warn = icons.diagnostics.Warn,
+                                info = icons.diagnostics.Info,
+                                hint = icons.diagnostics.Hint,
+                            },
+                        },
+                        { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+                        { "filename", path = 1, symbols = { modified = "  ", readonly = "", unnamed = "" } },
+                        -- stylua: ignore
+                    },
+                    lualine_x = {
+          -- stylua: ignore
+          {
+            function() return require("noice").api.status.command.get() end,
+            cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
+            color = Util.fg("Statement"),
+          },
+          -- stylua: ignore
+          {
+            function() return require("noice").api.status.mode.get() end,
+            cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
+            color = Util.fg("Constant"),
+          },
+          -- stylua: ignore
+          {
+            function() return "  " .. require("dap").status() end,
+            cond = function () return package.loaded["dap"] and require("dap").status() ~= "" end,
+            color = Util.fg("Debug"),
+          },
+                        {
+                            require("lazy.status").updates,
+                            cond = require("lazy.status").has_updates,
+                            color = Util.fg("Special"),
+                        },
+                        {
+                            "diff",
+                            symbols = {
+                                added = icons.git.added,
+                                modified = icons.git.modified,
+                                removed = icons.git.removed,
+                            },
+                        },
+                    },
+                    lualine_y = {
+                        { "progress", separator = " ", padding = { left = 1, right = 0 } },
+                        { "location", padding = { left = 0, right = 1 } },
+                    },
+                    lualine_z = {
+                        function()
+                            return " " .. os.date("%R")
+                        end,
+                    },
+                },
+                extensions = { "neo-tree", "lazy" },
+            }
+        end,
+    },
+}
