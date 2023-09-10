bcf4953950585d1535afd6af59e237d91b8c7e06
Sergio Laín <sergiolain11@gmail.com>
2023-09-10 20:07:48 +0200

    Changed zi function back to normal

--- .config/fish/functions/cdzi.fish
+++ .config/fish/functions/cdzi.fish
@@ -1,9 +1,7 @@
 function cdzi --description 'Open directories from zoxide in a new Kitty tab'
-    set selected_dir (zoxide query --list | fzf)
+    set selected_dir (zi)
 
     if test -n "$selected_dir"
-        set tab_title (basename "$selected_dir")
-        set new_tab_cmd "kitty @ launch --type=tab --tab-title \"$tab_title\" $selected_dir"
-        eval $new_tab_cmd
+        cd $selected_dir
     end
 end
