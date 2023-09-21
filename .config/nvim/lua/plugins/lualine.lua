fc5856e5a3b62f0103d5c577be2cf62b383cfbd1
Sergio Laín <sergiolain11@gmail.com>
2023-09-21 11:13:11 +0200

    Zellij config added, changed lualine to take out the clock because of zellij status bar one

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -68,15 +68,12 @@ return {
                             },
                         },
                     },
-                    lualine_y = {
 
-                        { "progress", separator = " ", padding = { left = 1, right = 0 } },
-                        { "location", padding = { left = 0, right = 1 } },
+                    lualine_y = {
+                        { "progress", separator = " ", padding = { left = 1, right = 1 } },
                     },
                     lualine_z = {
-                        function()
-                            return " " .. os.date("%R")
-                        end,
+                        { "location", padding = { left = 1, right = 1 } },
                     },
                 },
                 extensions = { "neo-tree", "lazy" },
