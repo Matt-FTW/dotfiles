db9d40a5ea9dced5a70322d10aae646d4bb91980
Sergio Laín <sergiolain11@gmail.com>
2023-10-11 12:36:35 +0200

    ✨ feat(fish): new function names

---
+++ .config/fish/functions/no.fish
@@ -0,0 +1,4 @@
+function no --wraps=floaterm --description 'alias no=floaterm'
+  floaterm $argv
+
+end
---
+++ .config/fish/functions/proc.fish
@@ -0,0 +1,4 @@
+function proc --wraps=sysz --description 'alias proc=sysz'
+  sysz $argv
+
+end
