0bf99009d7438f6918a8e5dd1e5318ed27273234
Sergio Laín <sergiolain11@gmail.com>
2023-10-13 20:17:04 +0200

    🐛 fix(nvim): adapt rainbow-delimeters to lua

--- .config/nvim/lua/plugins/rainbow-delimeters.lua
+++ .config/nvim/lua/plugins/rainbow-delimeters.lua
@@ -2,28 +2,19 @@ return {
     {
         "HiPhish/rainbow-delimiters.nvim",
         event = "VeryLazy",
-        config = function()
-            local rainbow_delimiters = require("rainbow-delimiters")
-
-            vim.g.rainbow_delimiters = {
-                strategy = {
-                    [""] = rainbow_delimiters.strategy["global"],
-                    vim = rainbow_delimiters.strategy["local"],
-                },
-                query = {
-                    [""] = "rainbow-delimiters",
-                    lua = "rainbow-blocks",
-                },
-                highlight = {
-                    "RainbowDelimiterRed",
-                    "RainbowDelimiterYellow",
-                    "RainbowDelimiterBlue",
-                    "RainbowDelimiterOrange",
-                    "RainbowDelimiterGreen",
-                    "RainbowDelimiterViolet",
-                    "RainbowDelimiterCyan",
-                },
-            }
+        config = function(_, opts)
+            require("rainbow-delimiters.setup")(opts)
         end,
+        opts = {
+            highlight = {
+                "RainbowDelimiterBlue",
+                "RainbowDelimiterYellow",
+                "RainbowDelimiterViolet",
+                "RainbowDelimiterOrange",
+                "RainbowDelimiterCyan",
+                "RainbowDelimiterRed",
+                "RainbowDelimiterGreen",
+            },
+        },
     },
 }
