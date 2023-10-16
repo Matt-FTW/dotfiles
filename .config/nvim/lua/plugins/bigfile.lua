980701768cbf3ae392ae7b13b48d72f538c4f5f7
Sergio Laín <sergiolain11@gmail.com>
2023-10-16 13:25:54 +0200

    ✨ feat(nvim): bigfile and lsp-timeout plugins added
    helping for those beffy files

---
+++ .config/nvim/lua/plugins/bigfile.lua
@@ -0,0 +1,16 @@
+return {
+  "LunarVim/bigfile.nvim",
+  event = "BufRead",
+  opts = {
+    filesize = 1, -- size of the file in MiB, the plugin round file sizes to the closest MiB
+    pattern = { "*" }, -- autocmd pattern or function see <### Overriding the detection of big files>
+    features = { -- features to disable
+      "indent_blankline",
+      "illuminate",
+      -- "lsp",
+      "syntax",
+      "vimopts",
+      "matchparen",
+    },
+  },
+}
