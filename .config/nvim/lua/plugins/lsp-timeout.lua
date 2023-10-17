caa7df8b64afdce153f54a8099255f6a9089d652
Sergio Laín <sergiolain11@gmail.com>
2023-10-17 12:41:39 +0200

    🐛 fix(nvim): typo on lsp-timeout

--- .config/nvim/lua/plugins/lsp-timeout.lua
+++ .config/nvim/lua/plugins/lsp-timeout.lua
@@ -3,7 +3,7 @@ return {
     "hinell/lsp-timeout.nvim",
     event = "BufRead",
     dependencies = { "neovim/nvim-lspconfig" },
-    enable = false,
+    enabled = false,
     init = function()
       vim.g["lsp-timeout-config"] = {
         startTimeout = 1000 * 1, -- ms before restart
