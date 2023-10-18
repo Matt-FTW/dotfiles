051c18103e7aa694833c567ba1dc860fdf6dc8e3
Sergio Laín <sergiolain11@gmail.com>
2023-10-18 12:00:02 +0200

    🎨 ux(nvim): new binds for rayso selected text

--- .config/nvim/lua/plugins/rayso.lua
+++ .config/nvim/lua/plugins/rayso.lua
@@ -3,7 +3,8 @@ return {
     "TobinPalmer/rayso.nvim",
     cmd = { "Rayso" },
     keys = {
-      { "<leader>cS", "<cmd>Rayso<cr>", desc = "Screenshot code" },
+      { "<leader>cS", "<cmd>Rayso<cr>", mode = { "n" }, desc = "Screenshot Code" },
+      { "<leader>cS", "<cmd>'<,'>Rayso<cr>", mode = { "v" }, desc = "Screenshot Selection" },
     },
     opts = {
       open_cmd = "librewolf",
