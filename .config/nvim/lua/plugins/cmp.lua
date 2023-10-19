0c8bbde1918d5521b7b1a00a3d90e873fc00534a
Sergio Laín <sergiolain11@gmail.com>
2023-10-19 13:26:23 +0200

    ✨ feat(nvim): new cmp sources
    still working to bring more

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -9,9 +9,26 @@ return {
         event = { "BufRead package.json" },
         opts = {},
       },
+      {
+        "petertriho/cmp-git",
+      },
+      {
+        "amarakon/nvim-cmp-fonts",
+      },
     },
-    opts = {
-      mapping = cmp.mapping.preset.insert({
+    opts = function(_, opts)
+      cmp.setup.filetype("gitcommit", {
+        sources = cmp.config.sources({
+          { name = "git" }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+        }),
+      })
+
+      cmp.setup.filetype("css", {
+        sources = cmp.config.sources({
+          { name = "fonts", option = { space_filter = "-" } }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+        }),
+      })
+      opts.mapping = cmp.mapping.preset.insert({
         ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
         ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
         ["<C-p>"] = cmp.mapping.scroll_docs(-4),
@@ -27,11 +44,11 @@ return {
           cmp.abort()
           fallback()
         end,
-      }),
-      window = {
+      })
+      opts.window = {
         completion = cmp.config.window.bordered(),
         documentation = cmp.config.window.bordered(),
-      },
-    },
+      }
+    end,
   },
 }
