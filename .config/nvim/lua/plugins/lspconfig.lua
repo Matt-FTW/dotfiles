d27b06709cfee2b3edb723001bd695c84b288fc7
Sergio Laín <sergiolain11@gmail.com>
2023-10-19 13:25:12 +0200

    ♻️ refactor(nvim): glance only for implementations and references and hint keyword added to the lspconfig

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -3,7 +3,7 @@ return {
     "neovim/nvim-lspconfig",
     init = function()
       local keys = require("lazyvim.plugins.lsp.keymaps").get()
-      keys[#keys + 1] = { "gd", "<CMD>Glance definitions<CR>", desc = "Goto definition" }
+      -- keys[#keys + 1] = { "gd", "<CMD>Glance definitions<CR>", desc = "Goto definition" }
       keys[#keys + 1] = { "gr", "<CMD>Glance references<CR>", desc = "References" }
       keys[#keys + 1] = { "gy", "<CMD>Glance type_definitions<CR>", desc = "Goto t[y]pe definitions" }
       keys[#keys + 1] = { "gI", "<CMD>Glance implementations<CR>", desc = "Goto implementations" }
@@ -26,6 +26,9 @@ return {
             },
           },
         },
+      },
+      inlay_hints = {
+        enabled = true,
       },
     },
   },
