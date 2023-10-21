75d9941d3e2bf0bcaf7d744ef86c17573fbab40d
Sergio Laín <sergiolain11@gmail.com>
2023-10-21 17:18:18 +0200

    ✨ feat(nvim): image extra, as well as moving the lsp and cmp to extras

--- .config/nvim/lua/plugins/extras/cmp.lua
+++ .config/nvim/lua/plugins/extras/cmp.lua
@@ -61,19 +61,6 @@ return {
       opts.mapping = cmp.mapping.preset.insert({
         ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
         ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-        ["<C-p>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-n>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<C-e>"] = cmp.mapping.abort(),
-        ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-        ["<S-CR>"] = cmp.mapping.confirm({
-          behavior = cmp.ConfirmBehavior.Replace,
-          select = true,
-        }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-        ["<C-CR>"] = function(fallback)
-          cmp.abort()
-          fallback()
-        end,
       })
       opts.window = {
         completion = cmp.config.window.bordered(),
---
+++ .config/nvim/lua/plugins/extras/image.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "edluffy/hologram.nvim",
+    lazy = true,
+    opts = {
+      auto_display = true, -- WIP automatic markdown image display, may be prone to breaking
+    },
+  },
+}
