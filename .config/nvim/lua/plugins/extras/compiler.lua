27bfa43c320114025c8ca56fba8b81b8270b2574
Sergio Laín <sergiolain11@gmail.com>
2023-10-21 17:17:35 +0200

    🎨 ux(nvim): added overseer bottom info instead of on the left

--- .config/nvim/lua/plugins/extras/compiler.lua
+++ .config/nvim/lua/plugins/extras/compiler.lua
@@ -26,6 +26,17 @@ return {
     },
   },
   {
+    "stevearc/overseer.nvim",
+    opts = {
+      task_list = {
+        direction = "bottom",
+        min_height = 15,
+        max_height = 15,
+        default_detail = 1,
+      },
+    },
+  },
+  {
     "folke/which-key.nvim",
     opts = {
       defaults = {
