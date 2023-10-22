b66c8dd1bf75d16171600920bf381c82723f3066
Sergio Laín <sergiolain11@gmail.com>
2023-10-22 18:42:18 +0200

    ⚙️ config(nvim): added codeium maps for next and prev

--- .config/nvim/lua/plugins/extras/codeium.lua
+++ .config/nvim/lua/plugins/extras/codeium.lua
@@ -9,10 +9,19 @@ return {
         return vim.fn["codeium#Accept"]()
       end, { expr = true })
 
+      vim.keymap.set("i", "<M-]>", function()
+        return vim.fn["codeium#CycleCompletions"](1)
+      end, { expr = true })
+
+      vim.keymap.set("i", "<M-[>", function()
+        return vim.fn["codeium#CycleCompletions"](-1)
+      end, { expr = true })
+
       vim.keymap.set("i", "<M-c>", function()
         return vim.fn["codeium#Clear"]()
       end, { expr = true })
-      vim.keymap.set("n", "<leader>ci", function()
+
+      vim.keymap.set("n", "<leader>cI", function()
         if vim.g.codeium_enabled == true then
           vim.cmd("CodeiumDisable")
         else
