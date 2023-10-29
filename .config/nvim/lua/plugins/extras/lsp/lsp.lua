723d261b136e91d80fc42feb153db24756ddb893
Sergio Laín <sergiolain11@gmail.com>
2023-10-29 23:50:02 +0100

    🎨 ux(nvim): nerd fonts icons for lsp lens

--- .config/nvim/lua/plugins/extras/lsp/lsp.lua
+++ .config/nvim/lua/plugins/extras/lsp/lsp.lua
@@ -166,7 +166,18 @@ return {
   {
     "VidocqH/lsp-lens.nvim",
     event = "BufReadPost",
-    opts = {},
+    opts = {
+      sections = {
+        definition = false,
+        references = function(count)
+          return " Ref: " .. count
+        end,
+        implements = function(count)
+          return "󱁤 Imp: " .. count
+        end,
+        git_authors = false,
+      },
+    },
     keys = {
       { "<leader>ue", "<cmd>LspLensToggle<cr>", desc = "Toggle Lsp Lens" },
     },
