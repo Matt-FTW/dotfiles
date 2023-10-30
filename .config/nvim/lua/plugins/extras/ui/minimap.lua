05d0157bb7bcda9117e7d7af3ddc737c8bd02858
Sergio Laín <sergiolain11@gmail.com>
2023-10-31 00:10:23 +0100

    ✨ feat(nvim): minimap and winshift extras

---
+++ .config/nvim/lua/plugins/extras/ui/minimap.lua
@@ -0,0 +1,22 @@
+return {
+  "echasnovski/mini.map",
+  opts = function()
+    local map = require("mini.map")
+    return {
+      symbols = {
+        encode = require("mini.map").gen_encode_symbols.dot("4x2"),
+      },
+      integrations = {
+        map.gen_integration.builtin_search(),
+        map.gen_integration.gitsigns(),
+        map.gen_integration.diagnostic(),
+      },
+      window = {
+        show_integration_count = false,
+      },
+    }
+  end,
+  keys = {
+    { "<leader>um", "<Cmd>lua MiniMap.toggle()<CR>", desc = "MiniMap" },
+  },
+}
---
+++ .config/nvim/lua/plugins/extras/util/winshift.lua
@@ -0,0 +1,6 @@
+return {
+  "sindrets/winshift.nvim",
+  cmd = "WinShift",
+  opts = { highlight_moving_win = true },
+  keys = { { "<leader>ws", "<CMD>WinShift<CR>", desc = "Win Shift/Swap" } },
+}
