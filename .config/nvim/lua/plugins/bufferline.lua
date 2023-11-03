63668d056a0b1fe8d76c931611107f52f5db500f
Sergio Laín <sergiolain11@gmail.com>
2023-11-03 11:31:31 +0100

    💄 style(nvim): indentation

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -1,24 +1,26 @@
 local macchiato = require("catppuccin.palettes").get_palette("macchiato")
 return {
-  {
-    "akinsho/bufferline.nvim",
-    opts = {
-      highlights = require("catppuccin.groups.integrations.bufferline").get({
-        styles = { "italic", "bold" },
-        custom = {
-          all = {
-            fill = { bg = macchiato.mantle },
-            background = { bg = macchiato.mantle },
-          },
+  "akinsho/bufferline.nvim",
+  keys = {
+    { "<leader>bH", "<cmd>BufferLineMovePrev<cr>", desc = "Move buffer prev" },
+    { "<leader>bL", "<cmd>BufferLineMoveNext<cr>", desc = "Move buffer next" },
+  },
+  opts = {
+    highlights = require("catppuccin.groups.integrations.bufferline").get({
+      styles = { "italic", "bold" },
+      custom = {
+        all = {
+          fill = { bg = macchiato.mantle },
+          background = { bg = macchiato.mantle },
         },
-      }),
-      options = {
-        buffer_close_icon = "",
-        modified_icon = "",
-        close_icon = "",
-        color_icons = true,
-        separator_style = "slope",
       },
+    }),
+    options = {
+      buffer_close_icon = "",
+      modified_icon = "",
+      close_icon = "",
+      color_icons = true,
+      separator_style = "slope",
     },
   },
 }
