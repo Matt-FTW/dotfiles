93b1a46cddc55bfd6c4778a97da845a0b599b778
Sergio Laín <sergiolain11@gmail.com>
2023-11-08 12:40:07 +0100

    ♻️ refactor(fish): repos function

--- .config/fish/functions/repos.fish
+++ .config/fish/functions/repos.fish
@@ -1,14 +1,11 @@
 function repos --description 'Open Git directories in ~/Repos with fzf in a new Kitty tab'
-    set selected_category (echo -e "Personal\nClass\nWork\nCommunity" | fzf --prompt "Select category: ")
+    set selected_dir (fd --type d --base-directory ~/Repos/ -H '^\.git$' -d 4 | sd '\.git/' '' | fzf)
 
-    if test -n "$selected_category"
-        set selected_dir (fd --type d --base-directory ~/Repos/"$selected_category"/ -H '^\.git$' -d 3 | sd '\.git/' '' | fzf)
-
-        if test -n "$selected_dir"
-            set tab_title (basename "$selected_dir")
-            set new_tab_cmd "kitty @ launch --type=tab --tab-title \"$tab_title\" ~/Repos/$selected_category/$selected_dir"
-            eval $new_tab_cmd
-        end
+    if test -n "$selected_dir"
+        set tab_title (basename "$selected_dir")
+        set new_tab_cmd "kitty @ launch --type=tab --tab-title \"$tab_title\" ~/Repos/$selected_category/$selected_dir"
+        eval $new_tab_cmd
     end
+
     commandline -f repaint
 end
