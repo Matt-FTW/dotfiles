1ef123907f9967ae20c9e6ddfdca8c9170e2bc7e
Sergio Laín <sergiolain11@gmail.com>
2023-11-12 23:24:57 +0100

    ✨ feat(nvim): changed projects.nvim to repo.nvim extra
    now it updates for all the new git repos added to the system

---
+++ .config/nvim/lua/plugins/extras/util/repo.lua
@@ -0,0 +1,33 @@
+local Util = require("lazyvim.util")
+
+return {
+  "cljoly/telescope-repo.nvim",
+  opts = {},
+  config = function(_, opts)
+    Util.on_load("telescope.nvim", function()
+      require("telescope").setup({
+        extensions = {
+          repo = {
+            list = {
+              fd_opts = {
+                "--no-ignore-vcs",
+              },
+              search_dirs = {
+                "~/Repos",
+              },
+            },
+          },
+        },
+      })
+      require("telescope").load_extension("repo")
+    end)
+  end,
+  keys = {
+    { "<leader>fp", "<cmd>Telescope repo list<cr>", desc = "Projects (~/Repos)" },
+    {
+      "<leader>fP",
+      "<cmd>lua require'telescope'.extensions.repo.list{search_dirs = { '~/' }}<cr>",
+      desc = "Projects (System)",
+    },
+  },
+}
