3a961a54228649acb76c7b3411a5edfa65148f7f
Sergio Laín <sergiolain11@gmail.com>
2023-11-14 11:23:28 +0100

    ♻️ refactor(nvim): changed db extra to sql, added notify option and edgy window module

--- .config/nvim/lua/plugins/extras/lang/sql.lua
+++ .config/nvim/lua/plugins/extras/lang/sql.lua
@@ -2,17 +2,48 @@ local sql_ft = { "sql", "mysql", "plsql" }
 
 return {
   {
+    "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "sql" })
+      end
+    end,
+  },
+  {
+    "folke/edgy.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.right = opts.right or {}
+      table.insert(opts.right, {
+        title = "Database",
+        ft = "dbui",
+        pinned = true,
+        open = function()
+          vim.cmd("DBUI")
+        end,
+      })
+
+      opts.bottom = opts.bottom or {}
+      table.insert(opts.bottom, {
+        title = "DB Query Result",
+        ft = "dbout",
+      })
+    end,
+  },
+  {
     "tpope/vim-dadbod",
+    cmd = { "DBUI", "DBUIToggle", "DBUIAddConnection", "DBUIFindBuffer" },
     dependencies = {
       "kristijanhusak/vim-dadbod-ui",
       { "kristijanhusak/vim-dadbod-completion", ft = sql_ft },
       { "jsborjesson/vim-uppercase-sql", ft = sql_ft },
     },
-    cmd = { "DBUI", "DBUIToggle", "DBUIAddConnection", "DBUIFindBuffer" },
     init = function()
-      vim.g.db_ui_use_nerd_fonts = true
       vim.g.db_ui_save_location = vim.fn.stdpath("data") .. "/db_ui"
+      vim.g.db_ui_use_nerd_fonts = true
       vim.g.db_ui_execute_on_save = false
+      vim.g.db_ui_use_nvim_notify = true
 
       vim.api.nvim_create_autocmd("FileType", {
         pattern = sql_ft,
