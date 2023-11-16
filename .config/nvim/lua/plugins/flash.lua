3adb2e39d71202f3de8289c7c1335f3a1e6e3e1c
Sergio Laín <sergiolain11@gmail.com>
2023-11-16 23:49:20 +0100

    🗑️ remove(nvim): flash unnecessary code

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -1,9 +1,5 @@
 return {
   "folke/flash.nvim",
-  event = "VeryLazy",
-  vscode = true,
-  ---@type Flash.Config
-  opts = {},
   keys = {
     {
       "s",
@@ -18,38 +14,6 @@ return {
         })
       end,
       desc = "Flash",
-    },
-    {
-      "S",
-      mode = { "n", "o", "x" },
-      function()
-        require("flash").treesitter()
-      end,
-      desc = "Flash Treesitter",
-    },
-    {
-      "r",
-      mode = "o",
-      function()
-        require("flash").remote()
-      end,
-      desc = "Remote Flash",
-    },
-    {
-      "R",
-      mode = { "o", "x" },
-      function()
-        require("flash").treesitter_search()
-      end,
-      desc = "Treesitter Search",
-    },
-    {
-      "<c-s>",
-      mode = { "c" },
-      function()
-        require("flash").toggle()
-      end,
-      desc = "Toggle Flash Search",
     },
   },
 }
