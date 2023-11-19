1d7cc9c1d0758843bbcbd294221fe0d47e72c22b
Sergio Laín <sergiolain11@gmail.com>
2023-11-19 02:54:42 +0100

    ♻️ refactor(nvim): breakpoints new bindings to adapt to the print extra added earlier

--- .config/nvim/lua/plugins/extras/dap/persistent-breakpoints.lua
+++ .config/nvim/lua/plugins/extras/dap/persistent-breakpoints.lua
@@ -1,12 +1,29 @@
 return {
-  "Weissle/persistent-breakpoints.nvim",
-  event = "BufReadPost",
-  keys = {
-    { "<leader>dd", "<cmd>PBClearAllBreakpoints<cr>", desc = "Delete All Breakpoints" },
-    { "<leader>dB", "<cmd>PBSetConditionalBreakpoint<cr>", desc = "Breakpoint Condition" },
-    { "<leader>db", "<cmd>PBToggleBreakpoint<cr>", desc = "Toggle Breakpoint" },
+  {
+    "Weissle/persistent-breakpoints.nvim",
+    event = "BufReadPost",
+    keys = {
+      { "<leader>dbd", "<cmd>PBClearAllBreakpoints<cr>", desc = "Delete All Breakpoints" },
+      { "<leader>dbB", "<cmd>PBSetConditionalBreakpoint<cr>", desc = "Breakpoint Condition" },
+      { "<leader>dbb", "<cmd>PBToggleBreakpoint<cr>", desc = "Toggle Breakpoint" },
+    },
+    opts = {
+      load_breakpoints_event = { "BufReadPost" },
+    },
   },
-  opts = {
-    load_breakpoints_event = { "BufReadPost" },
+  {
+    "folke/which-key.nvim",
+    opts = {
+      defaults = {
+        ["<leader>db"] = { name = "+breakpoints" },
+      },
+    },
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      { "<leader>dL", false },
+      { "<leader>dbl", "<cmd>Telescope dap list_breakpoints<CR>", desc = "List Breakpoints" },
+    },
   },
 }
