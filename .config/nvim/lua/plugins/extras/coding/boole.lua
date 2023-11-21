910e95e76415a12128182f0e14c5655351998f66
Sergio Laín <sergiolain11@gmail.com>
2023-11-21 10:47:44 +0100

    ✨ feat(nvim): added boole extra

---
+++ .config/nvim/lua/plugins/extras/coding/boole.lua
@@ -0,0 +1,25 @@
+return {
+  "nat-418/boole.nvim",
+  -- stylua: ignore
+  keys = {
+    { "<C-a>", "<cmd>Boole increment<cr>", expr = true, desc = "Increment" },
+    { "<C-x>", "<cmd>Boole decrement<cr>", expr = true, desc = "Decrement" },
+  },
+  opts = {
+    mappings = {
+      increment = "<C-a>",
+      decrement = "<C-x>",
+    },
+    -- User defined loops
+    additions = {
+      { "Foo", "Bar" },
+      { "tic", "tac", "toe" },
+    },
+    allow_caps_additions = {
+      { "enable", "disable" },
+      -- enable → disable
+      -- Enable → Disable
+      -- ENABLE → DISABLE
+    },
+  },
+}
