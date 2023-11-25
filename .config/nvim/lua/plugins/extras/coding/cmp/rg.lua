f854cccafd1956e7bdfc4c495f13598d836d4d67
Sergio Laín <sergiolain11@gmail.com>
2023-11-25 19:15:54 +0100

    ♻️ refactor(nvim): cmp addons moved to extras

---
+++ .config/nvim/lua/plugins/extras/coding/cmp/rg.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      {
+        "lukas-reineke/cmp-rg",
+      },
+    },
+    opts = function(_, opts)
+      table.insert(opts.sources, { name = "rg" })
+    end,
+  },
+}
---
+++ .config/nvim/lua/plugins/extras/coding/cmp/under-comparator.lua
@@ -0,0 +1,11 @@
+return {
+  "hrsh7th/nvim-cmp",
+  dependencies = {
+    {
+      "lukas-reineke/cmp-under-comparator",
+    },
+  },
+  opts = function(_, opts)
+    table.insert(opts.sorting.comparators, 4, require("cmp-under-comparator").under)
+  end,
+}
