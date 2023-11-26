cb68f38aebc66f116bbc95250b6b45fa0a6578af
Sergio Laín <sergiolain11@gmail.com>
2023-11-26 01:38:01 +0100

    🐛 fix(nvim): scrollbar and breadcrumbs extras now installs a different plugin depending on the version

--- .config/nvim/lua/plugins/extras/ui/scrollbar.lua
+++ .config/nvim/lua/plugins/extras/ui/scrollbar.lua
@@ -1,28 +1,26 @@
-local version = vim.fn.has("nvim-0.10")
-
--- if version then
---   return {
---     "lewis6991/satellite.nvim",
---     opts = {
---       excluded_filetypes = { "neo-tree", "alpha", "symbols-outline" },
---     },
---     event = "BufRead",
---   }
--- else
-return {
-  "dstein64/nvim-scrollview",
-  event = "BufReadPost",
-  keys = { { "<leader>uS", "<cmd>ScrollViewToggle<CR>", desc = "Toggle Scrollview" } },
-  config = function()
-    require("scrollview").setup({
+if vim.fn.has("nvim-0.10") == 1 then
+  return {
+    "lewis6991/satellite.nvim",
+    opts = {
       excluded_filetypes = { "neo-tree", "alpha", "symbols-outline" },
-      signs_column = 0,
-      winblend = 25,
-      diagnostics_error_symbol = "",
-      diagnostics_warn_symbol = "",
-      diagnostics_info_symbol = "",
-      diagnostics_hint_symbol = "",
-    })
-  end,
-}
--- end
+    },
+    event = "BufRead",
+  }
+else
+  return {
+    "dstein64/nvim-scrollview",
+    event = "BufReadPost",
+    keys = { { "<leader>uS", "<cmd>ScrollViewToggle<CR>", desc = "Toggle Scrollview" } },
+    config = function()
+      require("scrollview").setup({
+        excluded_filetypes = { "neo-tree", "alpha", "symbols-outline" },
+        signs_column = 0,
+        winblend = 25,
+        diagnostics_error_symbol = "",
+        diagnostics_warn_symbol = "",
+        diagnostics_info_symbol = "",
+        diagnostics_hint_symbol = "",
+      })
+    end,
+  }
+end
