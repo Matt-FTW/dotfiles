356a3d2894231bf12f6e2eb877b4f23aa7a53640
Sergio Laín <sergiolain11@gmail.com>
2023-11-28 13:19:38 +0100

    ⚙️ config(nvim): added autoindent treesitter integration

--- .config/nvim/lua/plugins/extras/coding/auto-indent.lua
+++ .config/nvim/lua/plugins/extras/coding/auto-indent.lua
@@ -1,5 +1,11 @@
 return {
   "vidocqh/auto-indent.nvim",
   event = "BufReadPost",
-  opts = {},
+  opts = {
+    ---@param lnum: number
+    ---@return number
+    indentexpr = function(lnum)
+      return require("nvim-treesitter.indent").get_indent(lnum)
+    end,
+  },
 }
