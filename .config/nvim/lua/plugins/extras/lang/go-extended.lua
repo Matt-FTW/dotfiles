e08e9f173e054ed11ce50c94a45ea9e15ad0eec2
Sergio Laín <sergiolain11@gmail.com>
2023-11-29 18:28:20 +0100

    🚀 perf(nvim): import lazyvim modules in the extended extras

--- .config/nvim/lua/plugins/extras/lang/go-extended.lua
+++ .config/nvim/lua/plugins/extras/lang/go-extended.lua
@@ -1,12 +1,15 @@
 return {
-  "ray-x/go.nvim",
-  dependencies = {
-    "ray-x/guihua.lua",
-    "neovim/nvim-lspconfig",
-    "nvim-treesitter/nvim-treesitter",
+  { import = "lazyvim.plugins.extras.lang.go" },
+  {
+    "ray-x/go.nvim",
+    dependencies = {
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    opts = {},
+    event = { "CmdlineEnter" },
+    ft = { "go", "gomod" },
+    build = ':lua require("go.install").update_all_sync()',
   },
-  opts = {},
-  event = { "CmdlineEnter" },
-  ft = { "go", "gomod" },
-  build = ':lua require("go.install").update_all_sync()',
 }
--- .config/nvim/lua/plugins/extras/lang/web/typescript-extended.lua
+++ .config/nvim/lua/plugins/extras/lang/web/typescript-extended.lua
@@ -1,4 +1,5 @@
 return {
+  { import = "lazyvim.plugins.extras.lang.typescript" },
   {
     "dmmulroy/tsc.nvim",
     opts = {
