c377fc69b5f695eeff36c7e3edb3c88478bf14fc
Sergio Laín <sergiolain11@gmail.com>
2023-11-30 20:43:42 +0100

    🚀 perf(nvim): omitted dial config

--- .config/nvim/lua/plugins/extras/coding/dial.lua
+++ .config/nvim/lua/plugins/extras/coding/dial.lua
@@ -1,21 +1,58 @@
 return {
   "monaqa/dial.nvim",
   -- stylua: ignore
-  keys = {
-    { "<A-a>", function() return require("dial.map").inc_normal() end, expr = true, desc = "Increment" },
-    { "<A-x>", function() return require("dial.map").dec_normal() end, expr = true, desc = "Decrement" },
+   keys = {
+    {
+      "<C-a>",
+      function()
+        require("dial.map").manipulate("increment", "normal")
+      end,
+    },
+    {
+      "<C-x>",
+      function()
+        require("dial.map").manipulate("decrement", "normal")
+      end,
+    },
+    {
+      "g<C-a>",
+      function()
+        require("dial.map").manipulate("increment", "gnormal")
+      end,
+    },
+    {
+      "g<C-x>",
+      function()
+        require("dial.map").manipulate("decrement", "gnormal")
+      end,
+    },
+    {
+      "<C-a>",
+      function()
+        require("dial.map").manipulate("increment", "visual")
+      end,
+      mode = "v",
+    },
+    {
+      "<C-x>",
+      function()
+        require("dial.map").manipulate("decrement", "visual")
+      end,
+      mode = "v",
+    },
+    {
+      "g<C-a>",
+      function()
+        require("dial.map").manipulate("increment", "gvisual")
+      end,
+      mode = "v",
+    },
+    {
+      "g<C-x>",
+      function()
+        require("dial.map").manipulate("decrement", "gvisual")
+      end,
+      mode = "v",
+    },
   },
-  config = function()
-    local augend = require("dial.augend")
-    require("dial.config").augends:register_group({
-      default = {
-        augend.integer.alias.decimal,
-        augend.integer.alias.hex,
-        augend.date.alias["%Y/%m/%d"],
-        augend.constant.alias.bool,
-        augend.constant.new({ elements = { "let", "const" } }),
-        augend.semver.alias.semver,
-      },
-    })
-  end,
 }
