22fedfbddf5485d60cb6019b018b1195721f92e2
Sergio Laín <sergiolain11@gmail.com>
2023-12-01 00:06:41 +0100

    ✨ feat(fish): added new functions (alias) for bob
    also included catppuccin themes from the latest installation with fisher

---
+++ .config/fish/functions/vimvl.fish
@@ -0,0 +1,4 @@
+function vimvl --wraps='bob use latest' --description 'alias vimvl=bob use latest'
+  bob use latest $argv
+        
+end
---
+++ .config/fish/functions/vimvn.fish
@@ -0,0 +1,4 @@
+function vimvn --wraps='bob use nightly' --description 'alias vimvn=bob use nightly'
+  bob use nightly $argv
+        
+end
