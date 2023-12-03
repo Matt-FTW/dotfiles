64b076dfa8740b2b71b0636dc89c52962c7c571f
Sergio Laín <sergiolain11@gmail.com>
2023-12-03 18:35:57 +0100

    🐛 fix(nvim): added csslsp to the extra
    didnt know why i didnt have it...

--- .config/nvim/lua/plugins/extras/lang/web/css.lua
+++ .config/nvim/lua/plugins/extras/lang/web/css.lua
@@ -12,6 +12,26 @@ return {
     opts = {
       servers = {
         cssmodules_ls = {},
+        cssls = {
+          lint = {
+            compatibleVendorPrefixes = "ignore",
+            vendorPrefix = "ignore",
+            unknownVendorSpecificProperties = "ignore",
+
+            -- unknownProperties = "ignore", -- duplicate with stylelint
+
+            duplicateProperties = "warning",
+            emptyRules = "warning",
+            importStatement = "warning",
+            zeroUnits = "warning",
+            fontFaceProperties = "warning",
+            hexColorLength = "warning",
+            argumentsInColorFunction = "warning",
+            unknownAtRules = "warning",
+            ieHack = "warning",
+            propertyIgnoredDueToDisplay = "warning",
+          },
+        },
       },
     },
   },
@@ -19,7 +39,7 @@ return {
     "williamboman/mason.nvim",
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, { "cssmodules-language-server", "rustywind" })
+      vim.list_extend(opts.ensure_installed, { "cssmodules-language-server", "css-lsp" })
     end,
   },
 }
