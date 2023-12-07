6a9ab71ee4ca84575a5d00f701ff4ffb6cbde776
Sergio Laín <sergiolain11@gmail.com>
2023-12-07 22:47:50 +0100

    ✨ feat(nvim): added glance before open hook

--- .config/nvim/lua/plugins/extras/lsp/glance.lua
+++ .config/nvim/lua/plugins/extras/lsp/glance.lua
@@ -13,6 +13,22 @@ return {
     border = {
       enable = true,
     },
+    hooks = {
+      before_open = function(results, open, jump, method)
+        local uri = vim.uri_from_bufnr(0)
+        if #results == 1 then
+          local target_uri = results[1].uri or results[1].targetUri
+
+          if target_uri == uri then
+            jump(results[1])
+          else
+            open(results)
+          end
+        else
+          open(results)
+        end
+      end,
+    },
   },
   keys = {
     { "gd", "<CMD>Glance definitions<CR>", desc = "Goto Definition" },
