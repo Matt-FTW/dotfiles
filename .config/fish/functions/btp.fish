22e75c5e7691fba2eb656be19f55890e934b2b17
Sergio Laín <sergiolain11@gmail.com>
2023-12-09 23:39:07 +0100

    📁 files(fish): changed bat functions to use the initial b instead of c for cat

---
+++ .config/fish/functions/btp.fish
@@ -0,0 +1,3 @@
+function btp --wraps='bat --paging=always' --description 'alias btp=bat --paging=always'
+    bat --paging=always $argv
+end
--- .config/fish/functions/ct.fish
+++
@@ -1,3 +0,0 @@
-function ct --wraps=bat --description 'alias ct=bat'
-    bat $argv
-end
