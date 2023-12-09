ce592dcd43a776306ec74236c9514c736dd42de4
Sergio Laín <sergiolain11@gmail.com>
2023-12-09 23:40:31 +0100

    ✨ feat(fish): added new functions for zoxide-eza and zoxide-neovim

--- .config/fish/functions/cl.fish
+++ .config/fish/functions/cl.fish
@@ -1,3 +1,3 @@
-function cl --wraps=clear --description 'alias cl=clear'
-    clear $argv
+function cl --wraps=clear --wraps='z && l' --description 'alias cl=z && l'
+    z $argv && l
 end
---
+++ .config/fish/functions/cv.fish
@@ -0,0 +1,3 @@
+function cv --wraps=clear --wraps='z && v' --description 'alias cv=z && v'
+    z $argv && v
+end
