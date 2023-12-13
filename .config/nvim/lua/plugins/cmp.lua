95901f4b80886be496ddb1ff6ec7031729f07e64
Sergio Laín <sergiolain11@gmail.com>
2023-12-13 12:30:12 +0100

    🚀 perf(nvim): cmp performance tweaked to appear instantly

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -6,11 +6,6 @@ return {
     { "<leader>cic", "<cmd>CmpStatus<CR>", desc = "Cmp Status" },
   },
   opts = function(_, opts)
-    local border = {
-      border = "rounded",
-      winhighlight = "Normal:Normal,FloatBorder:Normal,CursorLine:Visual,Search:None",
-    }
-
     opts.mapping = cmp.mapping.preset.insert({
       ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
       ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
@@ -32,9 +27,29 @@ return {
     })
 
     opts.window = {
-      completion = cmp.config.window.bordered(border),
-      documentation = cmp.config.window.bordered(border),
+      completion = {
+        border = "rounded",
+        winhighlight = "Normal:Normal,FloatBorder:Normal,CursorLine:Visual,Search:None",
+        scrollbar = false,
+        col_offset = -3,
+        side_padding = 1,
+      },
+      documentation = {
+        border = "rounded",
+        winhighlight = "Normal:Normal,FloatBorder:Normal,CursorLine:Visual,Search:None",
+        scrollbar = false,
+      },
     }
+
+    opts.performance = {
+      debounce = 20,
+      throttle = 20,
+      fetching_timeout = 20,
+      confirm_resolve_timeout = 20,
+      async_budget = 1,
+      max_view_entries = 50,
+    }
+
     cmp.setup.cmdline({ "/", "?" }, {
       mapping = cmp.mapping.preset.cmdline(),
       sources = {
