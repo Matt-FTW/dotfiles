ea8f38b8637cf506ed57b87b6ce64cc1f80610ea
Sergio Laín <sergiolain11@gmail.com>
2023-12-19 14:25:11 +0100

    ⚙️ config(nvim): enable spelling when not using vscode

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,6 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+if not vim.g.vscode then
+  vim.o.spell = true
+end
+
 vim.loader.enable()
 
 vim.g["rooter_cd_cmd"] = "lcd"
