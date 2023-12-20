c0fa54da1f3fe6caa63454f618f6e4ef1e008304
Sergio Laín <sergiolain11@gmail.com>
2023-12-20 13:34:14 +0100

    ⚙️ config(nvim): add icon-picker options to a submodule in whichkey

--- .config/nvim/lua/plugins/extras/util/icon-picker.lua
+++ .config/nvim/lua/plugins/extras/util/icon-picker.lua
@@ -1,13 +1,26 @@
 return {
-  "ziontee113/icon-picker.nvim",
-  dependencies = {
-    "stevearc/dressing.nvim",
-  },
-  opts = {
-    disable_legacy_commands = true,
-  },
+  {
+    "ziontee113/icon-picker.nvim",
+    dependencies = {
+      "stevearc/dressing.nvim",
+    },
+    opts = {
+      disable_legacy_commands = true,
+    },
   -- stylua: ignore
-  keys = {
-    { "<leader>si", "<cmd>IconPickerNormal<cr>", desc = "Icons" },
+    keys = {
+      { "<leader>sia", "<cmd>IconPickerNormal<cr>", desc = "All" },
+      { "<leader>sis", "<cmd>IconPickerNormal symbols<cr>", desc = "Symbols" },
+      { "<leader>sie", "<cmd>IconPickerNormal emoji<cr>", desc = "Emoji" },
+      { "<leader>sin", "<cmd>IconPickerNormal nerd_font_v3<cr>", desc = "Nerd Fonts" },
+    },
+  },
+  {
+    "folke/which-key.nvim",
+    opts = {
+      defaults = {
+        ["<leader>si"] = { name = "+icons" },
+      },
+    },
   },
 }
