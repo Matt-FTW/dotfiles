40fca97482831ffdfaf97ae9de030736d8baecd5
Sergio Laín <sergiolain11@gmail.com>
2023-12-22 10:32:55 +0100

    ⚙️ config(nvim): spider keymap

--- .config/nvim/lua/plugins/extras/coding/treesitter/spider.lua
+++ .config/nvim/lua/plugins/extras/coding/treesitter/spider.lua
@@ -5,6 +5,6 @@ return {
     { "w", "<cmd>lua require('spider').motion('w')<CR>", mode = { "n", "o", "x" }, desc = "Spider-w" },
     { "e", "<cmd>lua require('spider').motion('e')<CR>", mode = { "n", "o", "x" }, desc = "Spider-e" },
     { "b", "<cmd>lua require('spider').motion('b')<CR>", mode = { "n", "o", "x" }, desc = "Spider-b" },
-    { "ge", "<cmd>lua require('spider').motion('ge')<CR>", mode = { "n", "o", "x" }, desc = "Spider-ge" },
+    { "E", "<cmd>lua require('spider').motion('ge')<CR>", mode = { "n", "o", "x" }, desc = "Spider-E" },
   },
 }
