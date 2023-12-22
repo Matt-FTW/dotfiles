4a8a7cb91282f7db175b885270f7687d8c7a31de
Sergio Laín <sergiolain11@gmail.com>
2023-12-22 11:43:06 +0100

    ♻️ refactor(nvim): harpoon now on main page in which-key, as well as revamped some bindings and added others

--- .config/nvim/lua/plugins/extras/editor/harpoon.lua
+++ .config/nvim/lua/plugins/extras/editor/harpoon.lua
@@ -3,17 +3,20 @@ return {
     "ThePrimeagen/harpoon",
     --stylua: ignore
     keys = {
-      { "<leader>fa", function() require("harpoon.mark").add_file() end, desc = "Add File to Harpoon" },
-      { "<leader>fm", "<cmd>Telescope harpoon marks<CR>", desc = "Marks" },
-      { "<leader>1", function() require("harpoon.ui").nav_file(1) end, desc = "File 1" },
-      { "<leader>2", function() require("harpoon.ui").nav_file(2) end, desc = "File 2" },
-      { "<leader>3", function() require("harpoon.ui").nav_file(3) end, desc = "File 3" },
-      { "<leader>4", function() require("harpoon.ui").nav_file(4) end, desc = "File 4" },
-      { "<leader>5", function() require("harpoon.ui").nav_file(5) end, desc = "File 5" },
-      { "<leader>6", function() require("harpoon.ui").nav_file(6) end, desc = "File 6" },
-      { "<leader>7", function() require("harpoon.ui").nav_file(7) end, desc = "File 7" },
-      { "<leader>8", function() require("harpoon.ui").nav_file(8) end, desc = "File 8" },
-      { "<leader>9", function() require("harpoon.ui").nav_file(9) end, desc = "File 9" },
+      { "<leader>ha", function() require("harpoon.mark").add_file() end, desc = "Add Mark" },
+      { "<leader>hh", function() require("harpoon.ui").toggle_quick_menu() end, desc = "Menu" },
+      { "<leader>hf", "<cmd>Telescope harpoon marks<CR>", desc = "Files" },
+      { "<leader>h1", function() require("harpoon.ui").nav_file(1) end, desc = "File 1" },
+      { "<leader>h2", function() require("harpoon.ui").nav_file(2) end, desc = "File 2" },
+      { "<leader>h3", function() require("harpoon.ui").nav_file(3) end, desc = "File 3" },
+      { "<leader>h4", function() require("harpoon.ui").nav_file(4) end, desc = "File 4" },
+      { "<leader>h5", function() require("harpoon.ui").nav_file(5) end, desc = "File 5" },
+      { "<leader>h6", function() require("harpoon.ui").nav_file(6) end, desc = "File 6" },
+      { "<leader>h7", function() require("harpoon.ui").nav_file(7) end, desc = "File 7" },
+      { "<leader>h8", function() require("harpoon.ui").nav_file(8) end, desc = "File 8" },
+      { "<leader>h9", function() require("harpoon.ui").nav_file(9) end, desc = "File 9" },
+      { "]H", function() require("harpoon.ui").nav_next() end, desc = "Next Harpoon File" },
+      { "[H", function() require("harpoon.ui").nav_prev() end, desc = "Prev Harpoon File" },
     },
     opts = {
       globalsettings = {
@@ -31,7 +34,7 @@ return {
     "goolord/alpha-nvim",
     optional = true,
     opts = function(_, dashboard)
-      local button = dashboard.button("m", " " .. " Marks", "<cmd>Telescope harpoon marks<CR>")
+      local button = dashboard.button("h", "󱌧 " .. " Harpoon", "<cmd>Telescope harpoon marks<CR>")
       button.opts.hl = "AlphaButtons"
       button.opts.hl_shortcut = "AlphaShortcut"
       table.insert(dashboard.section.buttons.val, 5, button)
@@ -43,9 +46,9 @@ return {
     opts = function(_, opts)
       local harpoon = {
         action = "Telescope harpoon marks",
-        desc = " Marks",
-        icon = " ",
-        key = "m",
+        desc = " Harpoon",
+        icon = "󱌧 ",
+        key = "h",
       }
 
       harpoon.desc = harpoon.desc .. string.rep(" ", 43 - #harpoon.desc)
@@ -53,5 +56,13 @@ return {
 
       table.insert(opts.config.center, 5, harpoon)
     end,
+  },
+  {
+    "folke/which-key.nvim",
+    opts = {
+      defaults = {
+        ["<leader>h"] = { name = "+󱌧 harpoon" },
+      },
+    },
   },
 }
