f2cfbd61105224c2b3e21ea35fe49c36c2090e03
Sergio Laín <sergiolain11@gmail.com>
2023-12-22 12:44:48 +0100

    ♻️ refactor(nvim): personal copilot extra now imports the lazyvim one

--- .config/nvim/lua/plugins/extras/coding/ai/copilot.lua
+++ .config/nvim/lua/plugins/extras/coding/ai/copilot.lua
@@ -1,23 +1,26 @@
 return {
-  "zbirenbaum/copilot.lua",
-  event = "InsertEnter",
-  opts = {
-    suggestion = {
-      enabled = true,
-      auto_trigger = true,
-      keymap = {
-        accept = "<M-CR>",
-        accept_line = "<M-l>",
-        accept_word = "<M-k>",
-        next = "<M-]>",
-        prev = "<M-[>",
-        dismiss = "<M-c>",
+  {
+    import = "lazyvim.plugins.extras.coding.copilot",
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    event = "InsertEnter",
+    opts = {
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        keymap = {
+          accept = "<M-CR>",
+          accept_line = "<M-l>",
+          accept_word = "<M-k>",
+          next = "<M-]>",
+          prev = "<M-[>",
+          dismiss = "<M-c>",
+        },
       },
     },
-    panel = { enabled = false },
-    filetypes = {
-      markdown = true,
-      help = true,
+    keys = {
+      { "<leader>cI", "<cmd>Copilot toggle<cr>", desc = "Toggle IA" },
     },
   },
 }
