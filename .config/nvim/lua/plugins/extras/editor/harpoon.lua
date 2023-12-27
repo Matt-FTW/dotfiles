d5e9f6e0ae1f45e0cffc42e20838d1973d447f46
Sergio Laín <sergiolain11@gmail.com>
2023-12-27 10:52:51 +0100

    ♻️ refactor(nvim): migrate harpoon to v2

--- .config/nvim/lua/plugins/extras/editor/harpoon.lua
+++ .config/nvim/lua/plugins/extras/editor/harpoon.lua
@@ -1,24 +1,26 @@
 return {
   {
     "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
     --stylua: ignore
     keys = {
-      { "<leader>ha", function() require("harpoon.mark").add_file() end, desc = "Add Mark" },
-      { "<leader>hh", function() require("harpoon.ui").toggle_quick_menu() end, desc = "Menu" },
+      { "<leader>ha", function() require("harpoon"):list():append() end, desc = "Add Mark" },
+      { "<leader>hh", function() require("harpoon").ui:toggle_quick_menu(require("harpoon"):list()) end, desc = "Menu" },
       { "<leader>hf", "<cmd>Telescope harpoon marks<CR>", desc = "Files" },
-      { "<leader>h1", function() require("harpoon.ui").nav_file(1) end, desc = "File 1" },
-      { "<leader>h2", function() require("harpoon.ui").nav_file(2) end, desc = "File 2" },
-      { "<leader>h3", function() require("harpoon.ui").nav_file(3) end, desc = "File 3" },
-      { "<leader>h4", function() require("harpoon.ui").nav_file(4) end, desc = "File 4" },
-      { "<leader>h5", function() require("harpoon.ui").nav_file(5) end, desc = "File 5" },
-      { "<leader>h6", function() require("harpoon.ui").nav_file(6) end, desc = "File 6" },
-      { "<leader>h7", function() require("harpoon.ui").nav_file(7) end, desc = "File 7" },
-      { "<leader>h8", function() require("harpoon.ui").nav_file(8) end, desc = "File 8" },
-      { "<leader>h9", function() require("harpoon.ui").nav_file(9) end, desc = "File 9" },
-      { "]H", function() require("harpoon.ui").nav_next() end, desc = "Next Harpoon File" },
-      { "[H", function() require("harpoon.ui").nav_prev() end, desc = "Prev Harpoon File" },
-      { "<M-]>", function() require("harpoon.ui").nav_next() end, desc = "Next Harpoon File" },
-      { "<M-[>", function() require("harpoon.ui").nav_prev() end, desc = "Prev Harpoon File" },
+      { "<leader>h1", function() require("harpoon"):list():select(1) end, desc = "File 1" },
+      { "<leader>h2", function() require("harpoon"):list():select(2) end, desc = "File 2" },
+      { "<leader>h3", function() require("harpoon"):list():select(3) end, desc = "File 3" },
+      { "<leader>h4", function() require("harpoon"):list():select(4) end, desc = "File 4" },
+      { "<leader>h5", function() require("harpoon"):list():select(5) end, desc = "File 5" },
+      { "<leader>h6", function() require("harpoon"):list():select(6) end, desc = "File 6" },
+      { "<leader>h7", function() require("harpoon"):list():select(7) end, desc = "File 7" },
+      { "<leader>h8", function() require("harpoon"):list():select(8) end, desc = "File 8" },
+      { "<leader>h9", function() require("harpoon"):list():select(9) end, desc = "File 9" },
+      { "]H", function() require("harpoon"):list():next() end, desc = "Next Harpoon File" },
+      { "[H", function() require("harpoon"):list():prev() end, desc = "Prev Harpoon File" },
+      { "<M-]>", function() require("harpoon"):list():next() end, desc = "Next Harpoon File" },
+      { "<M-[>", function() require("harpoon"):list():prev() end, desc = "Prev Harpoon File" },
     },
     opts = {
       globalsettings = {
@@ -26,7 +28,9 @@ return {
         enter_on_sendcmd = true,
       },
     },
-    config = function()
+    config = function(_, opts)
+      require("harpoon").setup(opts)
+
       require("lazyvim.util").on_load("telescope.nvim", function()
         require("telescope").load_extension("harpoon")
       end)
