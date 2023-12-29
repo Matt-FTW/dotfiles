3dd5a489f6072b6f8c5eec6acb0059da11a81810
Sergio Laín <sergiolain11@gmail.com>
2023-12-30 00:31:01 +0100

    ♻️ refactor(nvim): sniprun keys added as well as compiler moved to editor extras instead of lsp

--- .config/nvim/lua/plugins/extras/editor/compiler.lua
+++ .config/nvim/lua/plugins/extras/editor/compiler.lua
@@ -16,6 +16,21 @@ return {
   },
   {
     "stevearc/overseer.nvim",
+    cmd = {
+      "OverseerOpen",
+      "OverseerClose",
+      "OverseerToggle",
+      "OverseerSaveBundle",
+      "OverseerLoadBundle",
+      "OverseerDeleteBundle",
+      "OverseerRunCmd",
+      "OverseerRun",
+      "OverseerInfo",
+      "OverseerBuild",
+      "OverseerQuickAction",
+      "OverseerTaskAction ",
+      "OverseerClearCache",
+    },
     opts = {
       task_list = {
         direction = "bottom",
