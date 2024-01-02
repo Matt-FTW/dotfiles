4fd723b637417811d201c3bc96b716106a8c2819
Sergio Laín <sergiolain11@gmail.com>
2024-01-02 11:09:31 +0100

    ✨ feat(nvim): add new keymaps for not yank when changing

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -66,9 +66,12 @@ map("n", "<C-c>", ":%y+<CR>", { desc = "Copy whole text to clipboard", silent = 
 -- Select all text
 map("n", "<C-a>", "gg<S-V>G", { desc = "Select all text", silent = true, noremap = true })
 
--- Better paste
--- remap "p" in visual mode to delete the highlighted text without overwriting your yanked/copied text, and then paste the content from the unnamed register.
-map("v", "p", '"_dP')
+map("v", "p", '"_dP', { desc = "Paste without overwriting" })
+
+map("n", "c", '"_c', { desc = "Change without yanking" })
+map("n", "C", '"_C', { desc = "Change without yanking" })
+map("x", "c", '"_c', { desc = "Change without yanking" })
+map("x", "C", '"_C', { desc = "Change without yanking" })
 
 map("n", "dd", function()
   local is_empty_line = vim.api.nvim_get_current_line():match("^%s*$")
