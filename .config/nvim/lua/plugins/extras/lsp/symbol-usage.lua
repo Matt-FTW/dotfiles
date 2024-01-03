7778f60f93d4fdaa777eb706d378bd8e23391128
Sergio Laín <sergiolain11@gmail.com>
2024-01-03 10:59:13 +0100

    ✨ feat(nvim): symbol usage is show if there is more than 1 reference

--- .config/nvim/lua/plugins/extras/lsp/symbol-usage.lua
+++ .config/nvim/lua/plugins/extras/lsp/symbol-usage.lua
@@ -7,5 +7,16 @@ return {
       return "BufRead"
     end
   end,
-  opts = {},
+  opts = {
+    vt_position = "end_of_line",
+    text_format = function(symbol)
+      if symbol.references then
+        local usage = symbol.references <= 1 and "usage" or "usages"
+        local num = symbol.references == 0 and "no" or symbol.references
+        return string.format(" 󰌹 %s %s", num, usage)
+      else
+        return ""
+      end
+    end,
+  },
 }
