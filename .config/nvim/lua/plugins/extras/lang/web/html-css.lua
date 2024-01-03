d15bfcfc668e1e9073c818e2cfadf0f4ae1351d9
Sergio Laín <sergiolain11@gmail.com>
2024-01-03 10:58:07 +0100

    ⚙️ config(nvim): merge html and css config together

--- .config/nvim/lua/plugins/extras/lang/web/html-css.lua
+++ .config/nvim/lua/plugins/extras/lang/web/html-css.lua
@@ -2,15 +2,19 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
-      if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, { "css", "scss" })
-      end
+      vim.list_extend(opts.ensure_installed, {
+        "html",
+        "css",
+        "scss",
+      })
     end,
   },
   {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
+        emmet_language_server = {},
+        html = {},
         cssmodules_ls = {},
         cssls = {
           lint = {
@@ -39,13 +43,19 @@ return {
     "williamboman/mason.nvim",
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, { "cssmodules-language-server", "css-lsp" })
+      vim.list_extend(opts.ensure_installed, {
+        "emmet-language-server",
+        "html-lsp",
+        "cssmodules-language-server",
+        "css-lsp",
+      })
     end,
   },
   {
     "luckasRanarison/nvim-devdocs",
     optional = true,
     ensure_installed = {
+      "html",
       "css",
       "sass",
     },
