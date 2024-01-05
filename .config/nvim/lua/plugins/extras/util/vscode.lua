1fb2926488b2329a6eff4903518b9c3ca9019d6c
Sergio Laín <sergiolain11@gmail.com>
2024-01-05 10:57:51 +0100

    ♻️ refactor(nvim): vscode extra now calls neovim-vscode module

--- .config/nvim/lua/plugins/extras/util/vscode.lua
+++ .config/nvim/lua/plugins/extras/util/vscode.lua
@@ -2,7 +2,9 @@ if not vim.g.vscode then
   return {}
 end
 
-vim.o.spell = false
+local Config = require("lazy.core.config")
+local Plugin = require("lazy.core.plugin")
+local vscode = require("vscode-neovim")
 
 -- Add any additional plugins in vscode, you can set vscode=true on a plugin spec.
 local enabled = {
@@ -14,17 +16,29 @@ local enabled = {
   "mini.surround",
   "nvim-treesitter",
   "nvim-treesitter-textobjects",
+  "nvim-various-textobjs",
   "nvim-ts-context-commentstring",
   "vim-repeat",
+  "yanky.nvim",
+  "highlight-undo.nvim",
+  "multicursors.nvim",
   "LazyVim",
 }
 
-local Config = require("lazy.core.config")
-local Plugin = require("lazy.core.plugin")
 Config.options.checker.enabled = false
 Config.options.change_detection.enabled = false
 Config.options.defaults.cond = function(plugin)
   return vim.tbl_contains(enabled, plugin.name) or plugin.vscode
+end
+
+vim.o.spell = false
+vim.notify = vscode.notify
+vim.g.clipboard = vim.g.vscode_clipboard
+
+local function vscode_action(cmd)
+  return function()
+    vscode.action(cmd)
+  end
 end
 
 -- Add some vscode specific keymaps
@@ -34,17 +48,17 @@ vim.api.nvim_create_autocmd("User", {
     -- find file
     vim.keymap.set("n", "<leader><space>", "<cmd>Find<cr>")
     -- find in files
-    vim.keymap.set("n", "<leader>/", [[<cmd>call VSCodeNotify('workbench.action.findInFiles')<cr>]])
+    vim.keymap.set("n", "<leader>/", vscode_action("workbench.action.findInFiles"))
     -- open symbol
-    vim.keymap.set("n", "<leader>ss", [[<cmd>call VSCodeNotify('workbench.action.gotoSymbol')<cr>]])
+    vim.keymap.set("n", "<leader>ss", vscode_action("workbench.action.gotoSymbol"))
     -- view problems
-    vim.keymap.set("n", "<leader>xx", [[<cmd>call VSCodeNotify('workbench.actions.view.problems')<cr>]])
+    vim.keymap.set("n", "<leader>xx", vscode_action("workbench.actions.view.problems"))
     -- open file explorer in left sidebar
-    vim.keymap.set("n", "<leader>e", [[<cmd>call VSCodeNotify('workbench.view.explorer')<cr>]])
+    vim.keymap.set("n", "<leader>e", vscode_action("workbench.view.explorer"))
     -- Code Action
-    vim.keymap.set("n", "<leader>ca", [[<cmd>call VSCodeNotify('editor.action.codeAction')<cr>]])
+    vim.keymap.set("n", "<leader>ca", vscode_action("editor.action.codeAction"))
     -- Open terminal
-    vim.keymap.set("n", "<leader>ft", [[<cmd>call VSCodeNotify('workbench.action.terminal.focus')<cr>]])
+    vim.keymap.set("n", "<leader>ft", vscode_action("workbench.action.terminal.focus"))
   end,
 })
 
@@ -60,6 +74,10 @@ return {
   },
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = { highlight = { enable = false } },
+    opts = {
+      highlight = {
+        enable = false,
+      },
+    },
   },
 }
