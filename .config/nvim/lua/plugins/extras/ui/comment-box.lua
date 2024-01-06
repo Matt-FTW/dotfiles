3b663d09c754bed9e85d3f8a40b4cec4ef5057d2
Sergio Laín <sergiolain11@gmail.com>
2024-01-06 20:29:40 +0100

    ⚙️ config(nvim): add keymaps to commentbox extra

--- .config/nvim/lua/plugins/extras/ui/comment-box.lua
+++ .config/nvim/lua/plugins/extras/ui/comment-box.lua
@@ -1,5 +1,23 @@
 return {
-  "LudoPinelli/comment-box.nvim",
-  event = "LazyFile",
-  opts = {},
+  {
+    "LudoPinelli/comment-box.nvim",
+    opts = {},
+    keys = {
+      { "<leader>acb", "<cmd>CBccbox<CR>", mode = { "n", "v" }, desc = "Comment Box" },
+      { "<leader>act", "<cmd>CBllline<CR>", mode = { "n", "v" }, desc = "Tiled Line" },
+      { "<leader>acl", "<cmd>CBline<CR>", desc = "Line" },
+      { "<leader>acm", "<cmd>CBllbox14<CR>", mode = { "n", "v" }, desc = "Marked" },
+      { "<leader>acc", "<cmd>CBcatalog<CR>", desc = "Catalog" },
+      { "<leader>acd", "<cmd>CBd<CR>", mode = { "n", "v" }, desc = "Delete Comment Style" },
+    },
+  },
+  {
+    "folke/which-key.nvim",
+    opts = {
+      defaults = {
+        ["<leader>a"] = { name = " annotation/snippets" },
+        ["<leader>ac"] = { name = "comments" },
+      },
+    },
+  },
 }
