20b4515d0f016790c8c3eee7a79ffbc083fad408
Sergio Laín <sergiolain11@gmail.com>
2024-01-07 01:05:54 +0100

    ✨ feat(nvim): cspell now uses condition, mason and a keybinding to add the word to a dictionary

--- .config/nvim/lua/plugins/extras/linting/cspell.lua
+++ .config/nvim/lua/plugins/extras/linting/cspell.lua
@@ -1,14 +1,29 @@
 return {
-  "mfussenegger/nvim-lint",
-  opts = {
-    linters_by_ft = {
-      ["*"] = { "cspell" },
-    },
-    linters = {
-      cspell = {
-        condition = function(ctx)
-          return vim.fs.find({ "cspell.json" }, { path = ctx.filename, upward = true })[1]
-        end,
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "cspell" })
+    end,
+  },
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters_by_ft = {
+        ["*"] = { "cspell" },
+      },
+      linters = {
+        cspell = {
+          condition = function(ctx)
+            if vim.fs.find({ "cspell.json" }, { path = ctx.filename, upward = true })[1] then
+              -- stylua: ignore
+              vim.keymap.set("n", "<leader>!", function() require("util.cspell").addWordToDictionary() end, { desc = "Add Word to Dictionary", silent = true })
+              return true
+            else
+              return false
+            end
+          end,
+        },
       },
     },
   },
