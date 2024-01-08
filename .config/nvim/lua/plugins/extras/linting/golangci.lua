3528240cf7dbc854f567e94e0d5ff109f1db177c
Sergio Laín <sergiolain11@gmail.com>
2024-01-09 00:01:45 +0100

    ⚙️ config(nvim): add golingcilint to gomod and gowork files

--- .config/nvim/lua/plugins/extras/linting/golangci.lua
+++ .config/nvim/lua/plugins/extras/linting/golangci.lua
@@ -9,8 +9,24 @@ return {
   {
     "mfussenegger/nvim-lint",
     opts = function(_, opts)
-      opts.linters_by_ft.go = opts.linters_by_ft.go or {}
-      table.insert(opts.linters_by_ft.go, "golangcilint")
+      --- Extend the conform plugin config and add given formatters
+      ---@param tbl table<string, conform.FormatterUnit[]> Table of filetype to formatters mappings
+      local function add_formatters(tbl)
+        for ft, formatters in pairs(tbl) do
+          if opts.formatters_by_ft[ft] == nil then
+            opts.formatters_by_ft[ft] = formatters
+          else
+            vim.list_extend(opts.formatters_by_ft[ft], formatters)
+          end
+        end
+      end
+
+      add_formatters({
+        ["go"] = { "golangcilint" },
+        ["gomod"] = { "golangcilint" },
+        ["gowork"] = { "golangcilint" },
+      })
+
       return opts
     end,
   },
