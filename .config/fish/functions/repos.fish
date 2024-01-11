6f4527d15df1ad27d9ff3852662dee6bd36fbf80
Sergio Laín <sergiolain11@gmail.com>
2024-01-11 10:17:07 +0100

    🚀 perf(fish): change logic of the repos command to just search in the repos folder with an exact depth without searching for a git folder
    as I always have repos, i dont have to worry about it, but if you do, make sure to uncomment the
    line of the previous command and comment this new one

--- .config/fish/functions/repos.fish
+++ .config/fish/functions/repos.fish
@@ -1,6 +1,7 @@
 function repos --description 'Open Git directories in ~/Repos with fzf in a new Kitty tab'
     set gfetch "gfetch -d churn contributors --no-color-palette --no-title"
-    set selected_dir (find ~/Repos -mindepth 2 -maxdepth 2 -type d -exec test -e '{}/.git' ';' -print -prune | fzf --preview "$gfetch {} && eza --long --header --icons --all --color=always --group-directories-first --hyperlink {}")
+    # set selected_dir (find ~/Repos -mindepth 2 -maxdepth 2 -type d -exec test -e '{}/.git' ';' -print -prune | fzf --preview "$gfetch {} && eza --long --header --icons --all --color=always --group-directories-first --hyperlink {}")
+    set selected_dir (fd --exact-depth 2 . ~/Repos/ | fzf --preview "$gfetch {} && eza --long --header --icons --all --color=always --group-directories-first --hyperlink {}")
 
     if test -n "$selected_dir"
         set tab_title (basename "$selected_dir")
