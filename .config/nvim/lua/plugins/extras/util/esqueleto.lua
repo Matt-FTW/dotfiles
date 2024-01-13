12a5f69ab9f53ec36e22fc44695a21ab5d0f82e6
Sergio Laín <sergiolain11@gmail.com>
2024-01-13 17:38:56 +0100

    ✨ feat(nvim): add vue component template

--- .config/nvim/lua/plugins/extras/util/esqueleto.lua
+++ .config/nvim/lua/plugins/extras/util/esqueleto.lua
@@ -15,6 +15,7 @@ return {
       "python",
       "lua",
       "html",
+      "vue",
     },
   },
 }
---
+++ .config/nvim/templates/vue/component.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "${filename}",
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
