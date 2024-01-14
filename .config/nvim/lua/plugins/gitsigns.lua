cd65cef6a580af08d47936dc7ca8c209b1472795
Sergio Laín <sergiolain11@gmail.com>
2024-01-14 13:23:22 +0100

    🗑️ remove(nvim): gitsigns current line blame
    it can be annoying sometimes

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -2,7 +2,6 @@ return {
   "lewis6991/gitsigns.nvim",
   opts = {
     yadm = { enable = true },
-    current_line_blame = true,
     current_line_blame_opts = {
       virt_text = true,
       virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
