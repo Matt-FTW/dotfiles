2b1877fb6d8db53b099fe3bbedad3ddcfb82e5aa
Sergio Laín <sergiolain11@gmail.com>
2024-01-17 10:28:15 +0100

    ✨ feat(nvim): add carbon-now extra
    replaces rayso for screenshotting code

---
+++ .config/nvim/lua/plugins/extras/editor/carbon-now.lua
@@ -0,0 +1,7 @@
+return {
+  "kristijanhusak/vim-carbon-now-sh",
+  cmd = { "CarbonNowSh" },
+  keys = {
+    { "<leader>cS", ":CarbonNowSh<cr>", mode = { "n", "v" }, desc = "Screenshot Code", silent = true },
+  },
+}
