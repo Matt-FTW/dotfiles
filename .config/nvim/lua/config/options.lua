36dac1d9bf44a0412e43dd9454747c69eaf9c07a
Sergio Laín <sergiolain11@gmail.com>
2024-01-23 10:06:40 +0100

    💄 style(nvim): use abrev on options.lua

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,22 +1,22 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-local opt = vim.g
+local o = vim.g
 
 -- Optimizations on startup
 vim.loader.enable()
 
 -- Define leader key
-opt.mapleader = " "
-opt.maplocalleader = "\\"
+o.mapleader = " "
+o.maplocalleader = "\\"
 
 -- Autoformat on save (Global)
-opt.autoformat = true
+o.autoformat = true
 
 -- Font
-opt.gui_font_default_size = 10
-opt.gui_font_size = opt.gui_font_default_size
-opt.gui_font_face = "JetBrainsMono Nerd Font"
+o.gui_font_default_size = 10
+o.gui_font_size = o.gui_font_default_size
+o.gui_font_face = "JetBrainsMono Nerd Font"
 
 -- Enable EditorConfig integration
-opt.editorconfig = true
+o.editorconfig = true
