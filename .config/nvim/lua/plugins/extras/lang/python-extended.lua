025725ecd0bcc045cf4c5bd379823efcb4c99600
Sergio Laín <sergiolain11@gmail.com>
2024-01-29 12:58:17 +0100

    ♻️ refactor(nvim): move pyrequirements extra to the python extended extra

--- .config/nvim/lua/plugins/extras/lang/python-extended.lua
+++ .config/nvim/lua/plugins/extras/lang/python-extended.lua
@@ -9,6 +9,31 @@ return {
     end,
   },
   {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "requirements" })
+      end
+    end,
+  },
+  {
+    "MeanderingProgrammer/py-requirements.nvim",
+    event = {
+      "BufRead requirements.txt",
+    },
+    dependencies = {
+      { "nvim-lua/plenary.nvim" },
+      {
+        "hrsh7th/nvim-cmp",
+        dependencies = {},
+        opts = function(_, opts)
+          table.insert(opts.sources, { name = "py-requirements" })
+        end,
+      },
+    },
+    opts = {},
+  },
+  {
     "luckasRanarison/nvim-devdocs",
     optional = true,
     ensure_installed = {
