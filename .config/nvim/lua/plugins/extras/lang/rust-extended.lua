4990ca6de58cb95194715ddef8e46c84bf3b36b1
Sergio Laín <sergiolain11@gmail.com>
2024-02-05 01:02:50 +0100

    ♻️ refactor(nvim): move bindings for package/dependency management into their own language submenu

--- .config/nvim/lua/plugins/extras/lang/rust-extended.lua
+++ .config/nvim/lua/plugins/extras/lang/rust-extended.lua
@@ -4,18 +4,6 @@ return {
     "simrat39/rust-tools.nvim",
     enabled = false,
   },
-  -- TODO: Look at crates lsp
-  -- {
-  --   "Saecki/crates.nvim",
-  --   opts = {
-  --     lsp = {
-  --       enabled = true,
-  --       on_attach = function(client, bufnr) end,
-  --       actions = true,
-  --       completion = true,
-  --     },
-  --   },
-  -- },
   {
     "mrcjkb/rustaceanvim",
     version = "^3", -- Recommended
@@ -84,6 +72,38 @@ return {
       table.insert(opts.formatters_by_ft.rust, "rustfmt")
       return opts
     end,
+  },
+  {
+    "Saecki/crates.nvim",
+    -- stylua: ignore
+    keys = {
+       { "<leader>prR", function() require("crates").reload() end, desc = "Reload" },
+
+       { "<leader>pru", function() require("crates").update_crate() end, desc = "Update Crate" },
+       { "<leader>pru", mode = "v", function() require("crates").update_crates() end, desc = "Update Crates" },
+       { "<leader>pra", function() require("crates").update_all_crates() end, desc = "Update All Crates" },
+
+       { "<leader>prU", function() require("crates").upgrade_crate() end, desc = "Upgrade Crate" },
+       { "<leader>prU", mode = "v", function() require("crates").upgrade_crates() end, desc = "Upgrade Crates" },
+       { "<leader>prA", function() require("crates").upgrade_all_crates() end, desc = "Upgrade All Crates" },
+
+       { "<leader>prt", function() require("crates").expand_plain_crate_to_inline_table() end, desc = "Extract into Inline Table" },
+       { "<leader>prT", function() require("crates").extract_crate_into_table() end, desc = "Extract into Table" },
+
+       { "<leader>prh", function() require("crates").open_homepage() end, desc = "Homepage" },
+       { "<leader>prr", function() require("crates").open_repository() end, desc = "Repo" },
+       { "<leader>prd", function() require("crates").open_documentation() end, desc = "Documentation" },
+       { "<leader>prc", function() require("crates").open_crates_io() end, desc = "Crates.io" },
+    },
+  },
+  {
+    "folke/which-key.nvim",
+    opts = {
+      defaults = {
+        ["<leader>p"] = { name = " packages/dependencies" },
+        ["<leader>pr"] = { name = "rust" },
+      },
+    },
   },
   {
     "luckasRanarison/nvim-devdocs",
