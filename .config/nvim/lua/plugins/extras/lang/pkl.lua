d7c01d5c32ad11ad2a3484b6ce50666e717aae76
Sergio Laín <sergiolain11@gmail.com>
2024-02-19 01:12:37 +0100

    🐛 fix(nvim): treesitter parser for pkl not installing correctly

--- .config/nvim/lua/plugins/extras/lang/pkl.lua
+++ .config/nvim/lua/plugins/extras/lang/pkl.lua
@@ -2,10 +2,9 @@ return {
   "apple/pkl-neovim",
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, { "pkl" })
-    end,
   },
+  build = function()
+    vim.cmd("TSInstall! pkl")
+  end,
   event = "BufRead *.pkl",
 }
