f3e9868129b93412567dea07424a7d8e75453d3b
Sergio Laín <sergiolain11@gmail.com>
2024-02-23 09:18:24 +0100

    🐛 fix(nvim): disable luasnip autocmd when inside vscode

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -96,12 +96,14 @@ ac({ "BufNewFile", "BufRead" }, {
 ac("ModeChanged", {
   pattern = "*",
   callback = function()
-    if
-      ((vim.v.event.old_mode == "s" and vim.v.event.new_mode == "n") or vim.v.event.old_mode == "i")
-      and require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
-      and not require("luasnip").session.jump_active
-    then
-      require("luasnip").unlink_current()
+    if not vim.g.vscode then
+      if
+        ((vim.v.event.old_mode == "s" and vim.v.event.new_mode == "n") or vim.v.event.old_mode == "i")
+        and require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
+        and not require("luasnip").session.jump_active
+      then
+        require("luasnip").unlink_current()
+      end
     end
   end,
 })
