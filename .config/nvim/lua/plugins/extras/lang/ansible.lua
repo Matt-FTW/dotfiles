99324ae4b488563474585295085473d040b84117
Sergio Laín <sergiolain11@gmail.com>
2024-02-27 09:53:28 +0100

    ✨ feat(nvim): add autodetection of ansible files

--- .config/nvim/lua/plugins/extras/lang/ansible.lua
+++ .config/nvim/lua/plugins/extras/lang/ansible.lua
@@ -9,6 +9,18 @@ return {
   },
   {
     "neovim/nvim-lspconfig",
+    init = function()
+      vim.filetype.add({
+        pattern = {
+          [".*/playbooks/.*%.yaml"] = "yaml.ansible",
+          [".*/playbooks/.*%.yml"] = "yaml.ansible",
+          [".*/roles/.*/tasks/.*%.yaml"] = "yaml.ansible",
+          [".*/roles/.*/tasks/.*%.yml"] = "yaml.ansible",
+          [".*/roles/.*/handlers/.*%.yaml"] = "yaml.ansible",
+          [".*/roles/.*/handlers/.*%.yml"] = "yaml.ansible",
+        },
+      })
+    end,
     opts = {
       servers = {
         ansiblels = {},
