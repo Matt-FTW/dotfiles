42470a05f48a9cb0f1094cad445ad06a0db758ff
Sergio Laín <sergiolain11@gmail.com>
2024-02-28 11:24:19 +0100

    ⚙️ config(nvim): change display position for nvim-recorder lualine module

--- .config/nvim/lua/plugins/extras/coding/recorder.lua
+++ .config/nvim/lua/plugins/extras/coding/recorder.lua
@@ -27,7 +27,7 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     opts = function(_, opts)
-      table.insert(opts.sections.lualine_x, 2, require("recorder").displaySlots)
+      table.insert(opts.sections.lualine_x, 1, require("recorder").displaySlots)
     end,
   },
 }
