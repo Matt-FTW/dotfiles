25bf8585f02d62a10c7488cff454c16d2b5bf2f1
Sergio Laín <sergiolain11@gmail.com>
2024-02-29 13:01:23 +0100

    🚀 perf(nvim): load neorg on norg files

--- .config/nvim/lua/plugins/extras/editor/docs/neorg.lua
+++ .config/nvim/lua/plugins/extras/editor/docs/neorg.lua
@@ -2,7 +2,7 @@ return {
   "nvim-neorg/neorg",
   build = ":Neorg sync-parsers",
   dependencies = { "nvim-lua/plenary.nvim" },
-  event = "VeryLazy",
+  ft = "norg",
   opts = {
     load = {
       ["core.defaults"] = {}, -- Loads default behaviour
