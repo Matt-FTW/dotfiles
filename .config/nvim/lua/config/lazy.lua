98e196b669672181f1cbd6d3c36d7e889af664f5
Sergio Laín <sergiolain11@gmail.com>
2024-03-07 21:47:39 +0100

    🗑️ remove(nvim): lazyrc; now using lazyvims one

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -12,7 +12,7 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     { import = "plugins" },
-    { import = "plugins.extras.util.lazyrc" },
+    { import = "lazyvim.plugins.extras.lazyrc" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
