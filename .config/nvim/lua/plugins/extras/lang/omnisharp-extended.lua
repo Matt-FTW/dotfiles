0c946be6bb36f332a4f0c191325b5c8dd2b45706
Sergio Laín <sergiolain11@gmail.com>
2024-03-07 21:50:45 +0100

    🗑️ remove(nvim): delete repeated code from the omnisharp extra

--- .config/nvim/lua/plugins/extras/lang/omnisharp-extended.lua
+++ .config/nvim/lua/plugins/extras/lang/omnisharp-extended.lua
@@ -1,45 +1,8 @@
 return {
   { import = "lazyvim.plugins.extras.lang.omnisharp" },
   {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, { "netcoredbg" })
-    end,
-  },
-  {
-    "mfussenegger/nvim-dap",
-    optional = true,
-    opts = function()
-      local dap = require("dap")
-      if not dap.adapters["netcoredbg"] then
-        require("dap").adapters["netcoredbg"] = {
-          type = "executable",
-          command = vim.fn.exepath("netcoredbg"),
-          args = { "--interpreter=vscode" },
-        }
-      end
-      for _, lang in ipairs({ "cs", "fsharp", "vb" }) do
-        if not dap.configurations[lang] then
-          dap.configurations[lang] = {
-            {
-              type = "netcoredbg",
-              name = "Launch file",
-              request = "launch",
-              ---@diagnostic disable-next-line: redundant-parameter
-              program = function()
-                return vim.fn.input("Path to dll: ", vim.fn.getcwd() .. "/", "file")
-              end,
-              cwd = "${workspaceFolder}",
-            },
-          }
-        end
-      end
-    end,
-  },
-  {
     "nvim-neotest/neotest",
-    optiona = true,
+    optional = true,
     dependencies = {
       "Issafalcon/neotest-dotnet",
     },
