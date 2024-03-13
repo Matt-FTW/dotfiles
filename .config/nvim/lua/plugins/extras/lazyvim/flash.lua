e47b17a38f7f427d168a55010886aec043c7b23f
Sergio Laín <sergiolain11@gmail.com>
2024-03-13 10:17:24 +0100

    ✨ feat(nvim): add a new binding for flash to search by the current word

--- .config/nvim/lua/plugins/extras/lazyvim/flash.lua
+++ .config/nvim/lua/plugins/extras/lazyvim/flash.lua
@@ -1,12 +1,26 @@
 return {
   "folke/flash.nvim",
   keys = {
-    -- stylua: ignore
     {
       "s",
       mode = { "n", "x", "o" },
-      function() require("flash").jump({ search = { mode = function(str) return "\\<" .. str end } }) end,
+      function()
+        require("flash").jump({
+          search = {
+            mode = function(str)
+              return "\\<" .. str
+            end,
+          },
+        })
+      end,
       desc = "Flash",
+    },
+    {
+      "<leader>*",
+      function()
+        require("flash").jump({ pattern = vim.fn.expand("<cword>") })
+      end,
+      desc = "Jump with current word",
     },
   },
 }
