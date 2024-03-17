20a6edd437eeaa63827d4f96189eb45d48cdf4a5
Sergio Laín <sergiolain11@gmail.com>
2024-03-17 02:55:49 +0100

    ✨ feat(fish): add homebrew and changed the config.fish order of sourcing

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,11 @@
-source ~/.config/fish/user_variables.fish
-source ~/.config/fish/abbreviations.fish
+eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+starship init fish | source
+zoxide init fish | source
 
 if status is-interactive
     atuin init fish | source
     source /usr/share/doc/find-the-command/ftc.fish
 end
 
-starship init fish | source
-zoxide init fish | source
+source ~/.config/fish/user_variables.fish
+source ~/.config/fish/abbreviations.fish
