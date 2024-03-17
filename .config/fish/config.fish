40e6a7dfa9275784b55183334a72366da8d36e7d
Sergio Laín <sergiolain11@gmail.com>
2024-03-18 00:12:47 +0100

    💬 comments(fish): comment brew line at the beginning of the config.fish file

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,5 @@
-eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+# Uncomment the next line to enable brew
+# eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv) 
 starship init fish | source
 zoxide init fish | source
 
