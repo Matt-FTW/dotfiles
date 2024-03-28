ca40f1394a502ba4babc580d90f63798b8e21993
Sergio Laín <sergiolain11@gmail.com>
2024-03-28 19:13:49 +0100

    🗑️ remove(nvim): stylelint mason installation on the tailwind extra

--- .config/nvim/lua/plugins/extras/lang/web/tailwind-extended.lua
+++ .config/nvim/lua/plugins/extras/lang/web/tailwind-extended.lua
@@ -2,13 +2,6 @@ return {
   { import = "lazyvim.plugins.extras.lang.tailwind" },
   { import = "plugins.extras.ui.inline-fold" },
   {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, { "stylelint" })
-    end,
-  },
-  {
     "MaximilianLloyd/tw-values.nvim",
     keys = {
       { "<leader>cT", "<cmd>TWValues<cr>", desc = "Tailwind CSS values" },
