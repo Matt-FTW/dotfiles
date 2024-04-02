4aef47abdaeef0c46959d6eef563edc7c5005a7a
Sergio Laín <sergiolain11@gmail.com>
2024-04-02 09:20:04 +0200

    🐛 fix(nvim): ansible filetype detection now works correctly

--- .config/nvim/lua/plugins/extras/lang/ansible-extended.lua
+++ .config/nvim/lua/plugins/extras/lang/ansible-extended.lua
@@ -1,3 +1,22 @@
+vim.filetype.add({
+  pattern = {
+    [".*/playbooks/.*%.yaml"] = "yaml.ansible",
+    [".*/playbooks/.*%.yml"] = "yaml.ansible",
+    [".*/roles/.*/tasks/.*%.yaml"] = "yaml.ansible",
+    [".*/roles/.*/tasks/.*%.yml"] = "yaml.ansible",
+    [".*/tasks/.*%.yaml"] = "yaml.ansible",
+    [".*/tasks/.*%.yml"] = "yaml.ansible",
+    [".*/vars/.*%.yaml"] = "yaml.ansible",
+    [".*/vars/.*%.yml"] = "yaml.ansible",
+    [".*/defaults/.*%.yaml"] = "yaml.ansible",
+    [".*/defaults/.*%.yml"] = "yaml.ansible",
+    [".*/handlers/.*%.yaml"] = "yaml.ansible",
+    [".*/handlers/.*%.yml"] = "yaml.ansible",
+    [".*/roles/.*/handlers/.*%.yaml"] = "yaml.ansible",
+    [".*/roles/.*/handlers/.*%.yml"] = "yaml.ansible",
+  },
+})
+
 return {
   { import = "plugins.extras.lang.yaml-extended" },
   { import = "lazyvim.plugins.extras.lang.ansible" },
@@ -6,21 +25,6 @@ return {
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
       vim.list_extend(opts.ensure_installed, { "ansible-language-server" })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    init = function()
-      vim.filetype.add({
-        pattern = {
-          [".*/playbooks/.*%.yaml"] = "yaml.ansible",
-          [".*/playbooks/.*%.yml"] = "yaml.ansible",
-          [".*/roles/.*/tasks/.*%.yaml"] = "yaml.ansible",
-          [".*/roles/.*/tasks/.*%.yml"] = "yaml.ansible",
-          [".*/roles/.*/handlers/.*%.yaml"] = "yaml.ansible",
-          [".*/roles/.*/handlers/.*%.yml"] = "yaml.ansible",
-        },
-      })
     end,
   },
   {
