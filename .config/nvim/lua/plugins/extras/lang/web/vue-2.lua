f4978df3436c8e9df6591027a74a7de960001d33
Sergio Laín <sergiolain11@gmail.com>
2024-04-06 00:11:32 +0200

    ✨ feat(nvim): add vue-2 and vue-3 extra instead of having only vue extra

---
+++ .config/nvim/lua/plugins/extras/lang/web/vue-2.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "vue" })
+      end
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        vuels = {},
+        volar = {
+          enabled = false,
+        },
+      },
+    },
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "vetur-vls" })
+    end,
+  },
+  {
+    "luckasRanarison/nvim-devdocs",
+    optional = true,
+    ensure_installed = {
+      "vue-3",
+    },
+  },
+}
