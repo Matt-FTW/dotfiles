a347b4dd94a511350520a41f6f6b695e9171b9b0
Sergio Laín <sergiolain11@gmail.com>
2024-04-07 13:57:31 +0200

    🐛 fix(nvim): cond some plugins for not loading when inside vscodium

--- .config/nvim/lua/plugins/extras/editor/codesnap.lua
+++ .config/nvim/lua/plugins/extras/editor/codesnap.lua
@@ -2,6 +2,7 @@ return {
   "mistricky/codesnap.nvim",
   build = "make",
   cmd = { "CodeSnap", "CodeSnapSave" },
+  cond = vim.g.vscode == nil,
   opts = {
     save_path = os.getenv("XDG_SCREENSHOTS_HOME") .. "/Codigo",
     title = "CodeSnap.nvim",
