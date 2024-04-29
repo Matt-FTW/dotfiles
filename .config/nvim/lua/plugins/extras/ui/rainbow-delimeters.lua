271c30386546cef940b259e9c2bdb03cb092ac36
Sergio Laín <sergiolain11@gmail.com>
2024-04-30 00:32:21 +0200

    🗑️ remove(nvim): dont pass custom opts to the rainbow-delimeters plugin

--- .config/nvim/lua/plugins/extras/ui/rainbow-delimeters.lua
+++ .config/nvim/lua/plugins/extras/ui/rainbow-delimeters.lua
@@ -2,16 +2,6 @@ return {
   "HiPhish/rainbow-delimiters.nvim",
   event = "LazyFile",
   config = function()
-    require("rainbow-delimiters.setup").setup({
-      highlight = {
-        "RainbowDelimiterBlue",
-        "RainbowDelimiterYellow",
-        "RainbowDelimiterViolet",
-        "RainbowDelimiterOrange",
-        "RainbowDelimiterCyan",
-        "RainbowDelimiterRed",
-        "RainbowDelimiterGreen",
-      },
-    })
+    require("rainbow-delimiters.setup").setup()
   end,
 }
