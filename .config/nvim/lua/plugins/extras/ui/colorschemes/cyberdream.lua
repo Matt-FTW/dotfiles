0d90bf7ab4b4d03740c4543f03df65c9d9c58561
Sergio Laín <sergiolain11@gmail.com>
2024-05-01 01:04:08 +0200

    ⚙️ config(nvim): add some opts to the cyberdream colorscheme

--- .config/nvim/lua/plugins/extras/ui/colorschemes/cyberdream.lua
+++ .config/nvim/lua/plugins/extras/ui/colorschemes/cyberdream.lua
@@ -2,5 +2,11 @@ return {
   "scottmckendry/cyberdream.nvim",
   lazy = false,
   name = "cyberdream",
-  opts = {},
+  opts = {
+    transparent = true,
+    italic_comments = true,
+    hide_fillchars = true,
+    borderless_telescope = true,
+    terminal_colors = true,
+  },
 }
