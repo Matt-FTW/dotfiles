0d14385b4c2bac62238e4785a413cb6c549b9ab8
Sergio Laín <sergio.lac@pm.me>
2024-05-05 13:39:43 +0200

    ⚙️ config(hypr): set ruff as default ruff lsp intead of ruff-lsp

--- .config/nvim/lua/plugins/extras/lang/python-extended.lua
+++ .config/nvim/lua/plugins/extras/lang/python-extended.lua
@@ -1,6 +1,7 @@
 -- LSP Server to use for Python.
 -- Set to "basedpyright" to use basedpyright instead of pyright.
 vim.g.lazyvim_python_lsp = "basedpyright"
+vim.g.lazyvim_python_ruff = "ruff"
 
 return {
   { import = "lazyvim.plugins.extras.lang.python" },
@@ -50,7 +51,7 @@ return {
             },
           },
         },
-        ruff = {
+        [vim.g.lazyvim_python_ruff] = {
           handlers = {
             ["textDocument/publishDiagnostics"] = function() end,
           },
