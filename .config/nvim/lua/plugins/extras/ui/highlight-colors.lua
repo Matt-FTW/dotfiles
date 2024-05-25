edf92ebed8a6170a18778c51d811bf0b903ee6a8
Sergio Laín <sergio.lac@pm.me>
2024-05-25 14:09:02 +0200

    ✨ feat(nvim): add keymap to toggle highlight-colors

--- .config/nvim/lua/plugins/extras/ui/highlight-colors.lua
+++ .config/nvim/lua/plugins/extras/ui/highlight-colors.lua
@@ -2,4 +2,8 @@ return {
   "brenoprata10/nvim-highlight-colors",
   event = "VeryLazy",
   opts = {},
+  -- stylua: ignore
+  keys = {
+    { "<leader>uc", function() require("nvim-highlight-colors").toggle() end, desc = "Toggle Highlight Colors" },
+  },
 }
