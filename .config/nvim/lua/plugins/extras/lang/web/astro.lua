b582113b99f68854cb9cc5a852c0c112ddef986c
Sergio Laín <sergio.lac@pm.me>
2024-05-31 21:08:01 +0200

    ✨ feat(nvim): import new lazyvim astro extra

--- .config/nvim/lua/plugins/extras/lang/web/astro.lua
+++ .config/nvim/lua/plugins/extras/lang/web/astro.lua
@@ -1,12 +1,5 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, { "astro" })
-      end
-    end,
-  },
+  { import = "lazyvim.plugins.extras.lang.astro" },
   {
     "neovim/nvim-lspconfig",
     opts = {
@@ -25,13 +18,6 @@ return {
   {
     "dmmulroy/ts-error-translator.nvim",
     opts = {},
-  },
-  {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, { "astro-language-server" })
-    end,
   },
   {
     "luckasRanarison/nvim-devdocs",
