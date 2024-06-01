79da319e9c36cf1e99ebfdaff63410b6a07708c7
Sergio Laín <sergio.lac@pm.me>
2024-06-01 21:45:02 +0200

    ♻️ refactor(nvim): personal svelte extra now imports lazyvim native one

--- .config/nvim/lua/plugins/extras/lang/web/svelte-extended.lua
+++ .config/nvim/lua/plugins/extras/lang/web/svelte-extended.lua
@@ -8,14 +8,7 @@ local inlay_hints_settings = {
 }
 
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, { "svelte" })
-      end
-    end,
-  },
+  { import = "lazyvim.plugins.extras.lang.svelte" },
   {
     "neovim/nvim-lspconfig",
     opts = {
@@ -40,13 +33,6 @@ return {
         },
       },
     },
-  },
-  {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, { "svelte-language-server" })
-    end,
   },
   {
     "dmmulroy/ts-error-translator.nvim",
