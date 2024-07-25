efc465ed67ce17d135da983f2ae54b3d36387462
Sergio Laín <sergio.lac@pm.me>
2024-07-25 18:53:16 +0200

    🐛 fix(nvim): hardcoded screenshot path for codesnap to work inside vscode and kitty scrollback

--- .config/nvim/lua/plugins/extras/editor/codesnap.lua
+++ .config/nvim/lua/plugins/extras/editor/codesnap.lua
@@ -2,14 +2,14 @@ return {
   "mistricky/codesnap.nvim",
   build = "make",
   cmd = { "CodeSnap", "CodeSnapSave" },
-  vscode = true,
+  vscode = false,
   keys = {
     { "<leader>cs", mode = "v", "<cmd>'<,'>CodeSnap<cr>", desc = "Screenshot (Clipboard)" },
     { "<leader>cS", mode = "v", "<cmd>'<,'>CodeSnapSave<cr>", desc = "Screenshot (Save)" },
   },
   cond = vim.env.KITTY_SCROLLBACK_NVIM == nil,
   opts = {
-    save_path = os.getenv("XDG_SCREENSHOTS_HOME") .. "/Codigo",
+    save_path = "/home/matt/Images/Capturas/Codigo", -- FIX: Need to find a way for this to work inside vscode and kitty
     title = "CodeSnap.nvim",
     code_font_family = "JetBrainsMono Nerd Font",
     watermark_font_family = "JetBrainsMono Nerd Font",
