b7400498ea6b9a01c9fe705ea7ee71ea69130b81
Sergio Laín <sergio.lac@pm.me>
2024-07-29 21:13:21 +0200

    🐛 fix(fish): bob nvim version manager

---
+++ .config/fish/functions/bob.fish
@@ -0,0 +1,9 @@
+function bob --wraps bob
+    command bob $argv
+    set used (cat ~/.local/share/bob/used)
+    set src ~/.local/share/bob/$used
+    set dest ~/.local/share/bob-nvim
+    test -L $dest
+    and unlink $dest
+    ln -s $src/nvim-linux64 $dest
+end
