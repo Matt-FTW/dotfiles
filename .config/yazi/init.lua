a1fa702d7bfe9f05c5dc58a24581188b2c3088d0
Sergio Laín <sergio.lac@pm.me>
2024-08-01 14:17:37 +0200

    ⚙️ config(yazi): plugin configurations

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,8 +1,22 @@
 -- Plugins
-require("starship"):setup()
+require("full-border"):setup({
+	type = ui.Border.ROUNDED,
+})
 require("zoxide"):setup({
 	update_db = true,
 })
 require("session"):setup({
 	sync_yanked = true,
+})
+require("starship"):setup()
+require("yaziline"):setup({
+	separator_style = "liney", -- "angly" | "curvy" | "liney" | "empty"
+	separator_open = "",
+	separator_close = "",
+	separator_open_thin = "",
+	separator_close_thin = "",
+	select_symbol = "",
+	yank_symbol = "󰆐",
+	filename_max_length = 24, -- trim when filename > 24
+	filename_trim_length = 6, -- trim 6 chars from both ends
 })
