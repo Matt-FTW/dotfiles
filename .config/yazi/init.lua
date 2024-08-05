0d6506b636e89a8aa8dae74277a478a76ec4b42a
Sergio Laín <sergio.lac@pm.me>
2024-08-05 17:35:42 +0200

    🗑️ remove(yazi): starship and lualine plugin

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -8,15 +8,4 @@ require("zoxide"):setup({
 require("session"):setup({
 	sync_yanked = true,
 })
-require("starship"):setup()
-require("yaziline"):setup({
-	separator_style = "liney", -- "angly" | "curvy" | "liney" | "empty"
-	separator_open = "",
-	separator_close = "",
-	separator_open_thin = "",
-	separator_close_thin = "",
-	select_symbol = "",
-	yank_symbol = "󰆐",
-	filename_max_length = 24, -- trim when filename > 24
-	filename_trim_length = 6, -- trim 6 chars from both ends
 })
