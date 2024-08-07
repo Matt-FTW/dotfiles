57310c2c51738b0a2ce35e0465c8636857ba3103
Sergio Laín <sergio.lac@pm.me>
2024-08-07 18:08:00 +0200

    ♻️ refactor(nvim): duplicate code

--- .config/nvim/lua/plugins/extras/coding/ai/gen.lua
+++ .config/nvim/lua/plugins/extras/coding/ai/gen.lua
@@ -1,3 +1,5 @@
+local prefix = "<leader>A"
+
 return {
   {
     "jellydn/gen.nvim",
@@ -34,29 +36,29 @@ return {
       }
     end,
     keys = {
-      { "<leader>ii", "<cmd>Gen<cr>", mode = { "n", "v" }, desc = "IA (Gen)" },
-      { "<leader>ig", "<cmd>Gen Generate<cr>", mode = { "n" }, desc = "Generate" },
-      { "<leader>ic", "<cmd>Gen Chat<cr>", mode = { "n" }, desc = "Chat" },
-      { "<leader>is", "<cmd>Gen Summarize<cr>", mode = { "n", "v" }, desc = "Summarize" },
-      { "<leader>ia", "<cmd>Gen Ask<cr>", mode = { "v" }, desc = "Ask" },
-      { "<leader>iH", "<cmd>Gen Change<cr>", mode = { "v" }, desc = "Change" },
-      { "<leader>iG", "<cmd>Gen Enhance_Grammar_Spelling<cr>", mode = { "v" }, desc = "Enhance Grammar Spelling" },
-      { "<leader>iw", "<cmd>Gen Enhance_Wording<cr>", mode = { "v" }, desc = "Enhance Wording" },
-      { "<leader>iC", "<cmd>Gen Make_Concise<cr>", mode = { "v" }, desc = "Make Concise" },
-      { "<leader>il", "<cmd>Gen Make_List<cr>", mode = { "v" }, desc = "Make List" },
-      { "<leader>it", "<cmd>Gen Make_Table<cr>", mode = { "v" }, desc = "Make Table" },
-      { "<leader>ir", "<cmd>Gen Review_Code<cr>", mode = { "v" }, desc = "Review Code" },
-      { "<leader>ie", "<cmd>Gen Enhance_Code<cr>", mode = { "v" }, desc = "Enhance Code" },
-      { "<leader>ih", "<cmd>Gen Change_Code<cr>", mode = { "v" }, desc = "Change Code" },
-      { "<leader>if", "<cmd>Gen Fix_Code<cr>", mode = { "v" }, desc = "Fix Code" },
-      { "<leader>iE", "<cmd>Gen Elaborate_Text<cr>", mode = { "v" }, desc = "Elaborate Text" },
+      { prefix .. "i", "<cmd>Gen<cr>", mode = { "n", "v" }, desc = "IA (Gen)" },
+      { prefix .. "g", "<cmd>Gen Generate<cr>", mode = { "n" }, desc = "Generate" },
+      { prefix .. "c", "<cmd>Gen Chat<cr>", mode = { "n" }, desc = "Chat" },
+      { prefix .. "s", "<cmd>Gen Summarize<cr>", mode = { "n", "v" }, desc = "Summarize" },
+      { prefix .. "a", "<cmd>Gen Ask<cr>", mode = { "v" }, desc = "Ask" },
+      { prefix .. "H", "<cmd>Gen Change<cr>", mode = { "v" }, desc = "Change" },
+      { prefix .. "G", "<cmd>Gen Enhance_Grammar_Spelling<cr>", mode = { "v" }, desc = "Enhance Grammar Spelling" },
+      { prefix .. "w", "<cmd>Gen Enhance_Wording<cr>", mode = { "v" }, desc = "Enhance Wording" },
+      { prefix .. "C", "<cmd>Gen Make_Concise<cr>", mode = { "v" }, desc = "Make Concise" },
+      { prefix .. "l", "<cmd>Gen Make_List<cr>", mode = { "v" }, desc = "Make List" },
+      { prefix .. "t", "<cmd>Gen Make_Table<cr>", mode = { "v" }, desc = "Make Table" },
+      { prefix .. "r", "<cmd>Gen Review_Code<cr>", mode = { "v" }, desc = "Review Code" },
+      { prefix .. "e", "<cmd>Gen Enhance_Code<cr>", mode = { "v" }, desc = "Enhance Code" },
+      { prefix .. "h", "<cmd>Gen Change_Code<cr>", mode = { "v" }, desc = "Change Code" },
+      { prefix .. "f", "<cmd>Gen Fix_Code<cr>", mode = { "v" }, desc = "Fix Code" },
+      { prefix .. "E", "<cmd>Gen Elaborate_Text<cr>", mode = { "v" }, desc = "Elaborate Text" },
     },
   },
   {
     "folke/which-key.nvim",
     opts = {
       spec = {
-        { "<leader>i", group = "IA (Gen)", icon = "󱚦 " },
+        { prefix, group = "IA (Gen)", icon = "󱚦 " },
       },
     },
   },
