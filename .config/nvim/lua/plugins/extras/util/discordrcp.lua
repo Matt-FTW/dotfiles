70203c27ba0a4b7055c336944e5d2b1c3be97a3f
Sergio Laín <sergio.lac@pm.me>
2024-08-17 12:35:09 +0200

    📦️ deps(nvim): update discordrpc extra with a modern plugin

--- .config/nvim/lua/plugins/extras/util/discordrcp.lua
+++ .config/nvim/lua/plugins/extras/util/discordrcp.lua
@@ -1,25 +1,26 @@
 return {
-  "andweeb/presence.nvim",
+  "IogaMaster/neocord",
   event = "VeryLazy",
   opts = {
-    auto_update = true,
-    neovim_image_text = "Chad Editor",
-    main_image = "file",
-    client_id = "793271441293967371",
-    log_level = nil,
-    debounce_timeout = 10,
-    enable_line_number = false,
-    blacklist = {},
-    buttons = true,
-    file_assets = {},
-    show_time = true,
+    logo = "auto", -- "auto" or url
+    logo_tooltip = nil, -- nil or string
+    main_image = "language", -- "language" or "logo"
+    client_id = "1157438221865717891", -- Use your own Discord application client id (not recommended)
+    log_level = nil, -- Log messages at or above this level (one of the following: "debug", "info", "warn", "error")
+    debounce_timeout = 10, -- Number of seconds to debounce events (or calls to `:lua package.loaded.presence:update(<filename>, true)`)
+    blacklist = {}, -- A list of strings or Lua patterns that disable Rich Presence if the current file name, path, or workspace matches
+    file_assets = {}, -- Custom file asset definitions keyed by file names and extensions (see default config at `lua/presence/file_assets.lua` for reference)
+    show_time = true, -- Show the timer
+    global_timer = false, -- if set true, timer won't update when any event are triggered
 
-    editing_text = "Editing %s",
-    file_explorer_text = "Browsing %s",
-    git_commit_text = "Committing changes",
-    plugin_manager_text = "Managing plugins",
-    reading_text = "Reading %s",
-    workspace_text = "Working on %s",
-    line_number_text = "Line %s out of %s",
+    -- Rich Presence text options
+    editing_text = "Editing %s", -- Format string rendered when an editable file is loaded in the buffer (either string or function(filename: string): string)
+    file_explorer_text = "Browsing %s", -- Format string rendered when browsing a file explorer (either string or function(file_explorer_name: string): string)
+    git_commit_text = "Committing changes", -- Format string rendered when committing changes in git (either string or function(filename: string): string)
+    plugin_manager_text = "Managing plugins", -- Format string rendered when managing plugins (either string or function(plugin_manager_name: string): string)
+    reading_text = "Reading %s", -- Format string rendered when a read-only or unmodifiable file is loaded in the buffer (either string or function(filename: string): string)
+    workspace_text = "Working on %s", -- Format string rendered when in a git repository (either string or function(project_name: string|nil, filename: string): string)
+    line_number_text = "Line %s out of %s", -- Format string rendered when `enable_line_number` is set to true (either string or function(line_number: number, line_count: number): string)
+    terminal_text = "Using Terminal", -- Format string rendered when in terminal mode.
   },
 }
