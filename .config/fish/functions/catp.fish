f5658704a6af723318a5238e60483a1a1240f9fb
Sergio Laín <sergio.lac@pm.me>
2024-09-02 00:04:54 +0200

    📁 files(fish): rename bat functions to use the whole word cat

---
+++ .config/fish/functions/catp.fish
@@ -0,0 +1,3 @@
+function catp --wraps='bat --paging=always' --description 'alias catp=bat --paging=always'
+    bat --paging=always $argv
+end
--- .config/fish/functions/ctp.fish
+++
@@ -1,3 +0,0 @@
-function ctp --wraps='bat --paging=always' --description 'alias ctp=bat --paging=always'
-    bat --paging=always $argv
-end
