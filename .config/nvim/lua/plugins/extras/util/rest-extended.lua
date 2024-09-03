7dc87763b9f2645f4f0fc820932e8cd76ade8410
Sergio Laín <sergio.lac@pm.me>
2024-09-03 11:55:49 +0200

    ⚙️ config(nvim): use rest-client included in lazyvim instead of custom one

---
+++ .config/nvim/lua/plugins/extras/util/rest-extended.lua
@@ -0,0 +1,16 @@
+return {
+  { import = "lazyvim.plugins.extras.util.rest" },
+  {
+    "mistweaverco/kulala.nvim",
+    opts = {
+      icons = {
+        inlay = {
+          loading = "󰔟",
+          done = " ",
+          error = " ",
+        },
+        lualine = " ",
+      },
+    },
+  },
+}
