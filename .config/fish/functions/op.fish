170dea8dbcb9de3f01f60a9260176fbc3a777347
Sergio Laín <sergio.lac@pm.me>
2024-10-03 14:53:04 +0200

    🔧 chore(fish): change obsidian vault path

--- .config/fish/functions/op.fish
+++ .config/fish/functions/op.fish
@@ -1,3 +1,3 @@
 function op --wraps='cd ~/Documents/Obsidian/obsidianVault' --description 'alias op=cd ~/Documents/Obsidian/obsidianVault'
-    cd ~/documents/obsidian-vaults/personal/ $argv
+    cd ~/documents/obsidian-vaults/personal-brain/ $argv
 end
