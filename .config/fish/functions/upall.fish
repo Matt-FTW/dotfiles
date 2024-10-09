c57dadeccfbd1fbeb0e0cfe2110303cbde44028d
Sergio Laín <sergio.lac@pm.me>
2024-10-09 12:54:21 +0200

    🐛 fix(fish): topgrade doesnt upgrade itself

--- .config/fish/functions/upall.fish
+++ .config/fish/functions/upall.fish
@@ -1,6 +1,6 @@
 function upall --wraps=topgrade --description 'alias upall=topgrade'
     if type -f topgrade &>/dev/null
-        topgrade -k $argv
+        topgrade -k --no-self-update $argv
     else
         missing_package topgrade
     end
