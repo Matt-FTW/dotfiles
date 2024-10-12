a1702602bcc0ab07aa3f08ad0bbbc6bfc4b59792
Sergio Laín <sergio.lac@pm.me>
2024-10-13 01:24:06 +0200

    🐛 fix(fish): function to open yazi

--- .config/fish/functions/fe.fish
+++ .config/fish/functions/fe.fish
@@ -1,12 +1,8 @@
 function fe
-    if type -f yazi &>/dev/null
-        set tmp (mktemp -t "yazi-cwd.XXXXX")
-        yazi --cwd-file="$tmp"
-        if set cwd (cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-            cd -- "$cwd"
-        end
-        rm -f -- "$tmp"
-    else
-        missing_package yazi
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
     end
+    rm -f -- "$tmp"
 end
