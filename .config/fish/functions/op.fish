c0066adf22341d6e514d9067a95bfff7764070e7
Sergio Laín <sergio.lac@pm.me>
2024-10-19 13:24:40 +0200

    🔧 chore(dir): rename obsidian vaults dir

--- .config/fish/functions/op.fish
+++ .config/fish/functions/op.fish
@@ -1,3 +1,3 @@
 function op --wraps='cd ~/Documents/Obsidian/obsidianVault' --description 'alias op=cd ~/Documents/Obsidian/obsidianVault'
-    cd ~/documents/obsidian-vaults/personal-brain/ $argv
+    cd ~/documents/obsidian/personal-brain/ $argv
 end
