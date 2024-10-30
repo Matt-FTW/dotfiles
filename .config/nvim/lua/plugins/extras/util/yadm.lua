c68f85440c6bd59c9a721b047e2aaa515fddbe0d
Sergio Laín <sergio.lac@pm.me>
2024-10-31 00:31:59 +0100

    🐛 fix(nvim): yadm repo link

--- .config/nvim/lua/plugins/extras/util/yadm.lua
+++ .config/nvim/lua/plugins/extras/util/yadm.lua
@@ -1,10 +1,14 @@
 return {
-  { "seanbreckenridge/gitsigns-yadm.nvim" },
+  { "purarue/gitsigns-yadm.nvim" },
   {
     "lewis6991/gitsigns.nvim",
     opts = {
       _on_attach_pre = function(_, callback)
-        require("gitsigns-yadm").yadm_signs(callback)
+        if vim.fn.executable("yadm") == 1 then
+          require("gitsigns-yadm").yadm_signs(callback)
+        else
+          callback()
+        end
       end,
     },
   },
