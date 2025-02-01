1b3ed7466efcc423541b8e40ff1020f04dd5817e
Sergio Laín <sergio.lac@pm.me>
2025-02-02 00:41:26 +0100

    ✨ feat(nvim): toggle lazygit when hitting q

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -113,3 +113,15 @@ ac("BufWritePre", {
     vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
   end,
 })
+
+-- Toggle lazygit instead of closing
+vim.api.nvim_create_autocmd("TermOpen", {
+  pattern = "*",
+  callback = function()
+    local term_title = vim.b.term_title
+    if term_title and term_title:match("lazygit") then
+      -- Create lazygit specific mappings
+      vim.keymap.set("t", "q", "<cmd>close<cr>", { buffer = true })
+    end
+  end,
+})
