e588ac84823d9e0be06c92a21eba14dc24caa861
Sergio Laín <sergio.lac@pm.me>
2025-02-02 00:44:29 +0100

    ✨ feat(nvim): add new keymaps to the snacks picker

--- .config/nvim/lua/plugins/extras/util/snacks-extended.lua
+++ .config/nvim/lua/plugins/extras/util/snacks-extended.lua
@@ -32,4 +32,11 @@ return {
       },
     },
   },
+  -- stylua: ignore
+  keys = {
+    { "<leader>fz", function() Snacks.picker.zoxide() end, desc = "Zoxide" },
+    { "<leader>S", function() Snacks.picker.spelling() end, desc = "Spelling" },
+    { "<leader>gB", function() Snacks.picker.git_branches() end, desc = "Git Branches" },
+    { "<leader>go", function() Snacks.gitbrowse() end, desc = "Git Open Line" },
+  },
 }
