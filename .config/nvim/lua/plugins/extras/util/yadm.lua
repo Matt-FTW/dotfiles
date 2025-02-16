ac65129f5b8aefb75db1bffa88ac1267ffca86c0
Sergio Laín <sergio.lac@pm.me>
2025-02-16 01:03:35 +0100

    🐛 fix(nvim): yadm extra

--- .config/nvim/lua/plugins/extras/util/yadm.lua
+++ .config/nvim/lua/plugins/extras/util/yadm.lua
@@ -1,12 +1,16 @@
 return {
-  { "purarue/gitsigns-yadm.nvim" },
   {
     "lewis6991/gitsigns.nvim",
+    dependencies = {
+      "purarue/gitsigns-yadm.nvim",
+    },
     opts = {
-      _on_attach_pre = function(_, callback)
+      _on_attach_pre = function(bufnr, callback)
         if vim.fn.executable("yadm") == 1 then
-          require("gitsigns-yadm").yadm_signs(callback)
+          require("gitsigns-yadm").yadm_signs(callback, { bufnr = bufnr })
         else
+          -- if optionally disabling the plugin, make sure to call
+          -- 'callback' with no arguments
           callback()
         end
       end,
