0a9054b83e2c8fec6c4aab7f8c4efa016498e944
Sergio Laín <sergio.lac@pm.me>
2025-03-13 00:22:55 +0100

    ✨ feat(nvim): markdownlint-cli2 default rule file

--- .config/nvim/lua/plugins/extras/lang/markdown-extended.lua
+++ .config/nvim/lua/plugins/extras/lang/markdown-extended.lua
@@ -36,6 +36,16 @@ return {
     },
   },
   {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters = {
+        ["markdownlint-cli2"] = {
+          prepend_args = { "--config", os.getenv("HOME") .. "/.config/nvim/rules/.markdownlint-cli2.yaml", "--" },
+        },
+      },
+    },
+  },
+  {
     "MeanderingProgrammer/render-markdown.nvim",
     opts = {
       preset = "lazy",
