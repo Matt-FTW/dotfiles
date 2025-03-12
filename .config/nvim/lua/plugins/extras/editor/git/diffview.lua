95dc18da5bbbda8e6bd966920950f4af2a9477da
Sergio Laín <sergio.lac@pm.me>
2025-03-13 00:23:22 +0100

    ✨ feat(nvim): diffview helper function to toggle the view easily

--- .config/nvim/lua/plugins/extras/editor/git/diffview.lua
+++ .config/nvim/lua/plugins/extras/editor/git/diffview.lua
@@ -1,11 +1,20 @@
 local prefix = "<leader>gC"
+local function toggle_diffview(cmd)
+  if next(require("diffview.lib").views) == nil then
+    vim.cmd(cmd)
+  else
+    vim.cmd("DiffviewClose")
+  end
+end
 
 return {
   {
     "sindrets/diffview.nvim",
+    -- stylua: ignore
     keys = {
-      { "<leader>gD", "<cmd>DiffviewFileHistory<CR>", desc = "Diff Repo" },
-      { "<leader>gd", "<cmd>DiffviewOpen<CR>", desc = "Diff View" },
+      { "<leader>gD", function() toggle_diffview("DiffviewFileHistory") end, desc = "Diff Repo" },
+      { "<leader>gd", function() toggle_diffview("DiffviewOpen") end, desc = "Diff View" },
+      { "<leader>gF", function() toggle_diffview("DiffviewFileHistory %") end, desc = "Diff Current File" },
     },
     opts = function(_, opts)
       local actions = require("diffview.actions")
