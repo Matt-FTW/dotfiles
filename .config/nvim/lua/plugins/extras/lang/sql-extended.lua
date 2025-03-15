82bb150787914a12654dc41ddd4477d0eb72774b
Sergio Laín <sergio.lac@pm.me>
2025-03-15 16:38:47 +0100

    🐛 fix(nvim): sql extra now imports sql from lazyvim correctly

--- .config/nvim/lua/plugins/extras/lang/sql-extended.lua
+++ .config/nvim/lua/plugins/extras/lang/sql-extended.lua
@@ -1,37 +1,7 @@
-local sql_ft = { "sql", "mysql", "plsql" }
 local prefix = "<leader>D"
 
 return {
-  { import = "lazyvim.plugins.extras.lang.python" },
-  {
-    "kristijanhusak/vim-dadbod-ui",
-    dependencies = {
-      { "jsborjesson/vim-uppercase-sql", ft = sql_ft },
-    },
-    keys = {
-      { prefix .. "a", "<cmd>DBUIAddConnection<cr>", desc = "Add Connection" },
-      { prefix .. "u", "<cmd>DBUIToggle<cr>", desc = "Toggle UI" },
-      { prefix .. "f", "<cmd>DBUIFindBuffer<cr>", desc = "Find Buffer" },
-      { prefix .. "r", "<cmd>DBUIRenameBuffer<cr>", desc = "Rename Buffer" },
-      { prefix .. "q", "<cmd>DBUILastQueryInfo<cr>", desc = "Last Query Info" },
-    },
-  },
-  {
-    "folke/which-key.nvim",
-    opts = {
-      spec = {
-        { prefix, group = "database", icon = " " },
-      },
-    },
-  },
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "sqlls",
-      },
-    },
-  },
+  { import = "lazyvim.plugins.extras.lang.sql" },
   {
     "neovim/nvim-lspconfig",
     opts = {
