fabd1695f91af103665bfc223d2ce12660cffc6c
Sergio Laín <sergio.lac@pm.me>
2025-04-20 14:06:58 +0200

    🐛 fix(fish): atuin up binding

--- .config/fish/conf.d/atuin.fish
+++ .config/fish/conf.d/atuin.fish
@@ -1,5 +1,19 @@
+set -x ATUIN_NOBIND true
+
 if status is-interactive
     if type -q atuin
         atuin init fish | source
     end
+end
+
+bind \cr _atuin_search
+bind up _atuin_bind_up
+bind \eOA _atuin_bind_up
+bind \e\[A _atuin_bind_up
+
+if bind -M insert >/dev/null 2>&1
+    bind -M insert \cr _atuin_search
+    bind -M insert up _atuin_bind_up
+    bind -M insert \eOA _atuin_bind_up
+    bind -M insert \e\[A _atuin_bind_up
 end
