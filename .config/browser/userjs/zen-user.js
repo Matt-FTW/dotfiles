deb70d53a89dc806095db681e968dbb91aceceec
Sergio Laín <sergio.lac@pm.me>
2025-05-24 20:39:28 +0200

    ⚙️ config(browser): disable zen transparent

--- .config/browser/userjs/zen-user.js
+++ .config/browser/userjs/zen-user.js
@@ -83,5 +83,5 @@ user_pref("zen.tabs.show-newtab-vertical", false);
 user_pref("zen.urlbar.behavior", "float");
 user_pref("zen.view.compact.hide-toolbar", true);
 user_pref("zen.workspaces.force-container-workspace", true);
-user_pref("zen.workspaces.open-new-tab-if-last-unpinned-tab-is-closed", false);
 user_pref("zen.glance.open-essential-external-links", false);
+user_pref("browser.tabs.allow_transparent_browser", false);
