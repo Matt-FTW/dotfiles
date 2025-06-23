91b02ef3d59cf9192dcba71a7e8c599bd19af07e
Sergio Laín <sergio.lac@pm.me>
2025-06-23 02:13:57 +0200

    feat(obsidian): polish tmuxp and tmux-notes-switch script

--- .config/fish/functions/op.fish
+++ .config/fish/functions/op.fish
@@ -1,3 +1,3 @@
 function op --wraps='cd ~/Documents/Obsidian/obsidianVault' --description 'alias op=cd ~/Documents/Obsidian/obsidianVault'
-    cd ~/documents/obsidian/personal-brain/ $argv
+    tmuxp load -y notes-tasks
 end
