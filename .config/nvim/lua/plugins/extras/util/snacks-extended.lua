5ed6cf8ce86e779b095c3386afbb998518264ebe
Sergio Laín <sergio.lac@pm.me>
2025-06-23 02:22:17 +0200

    feat(nvim): new snacks bindings

--- .config/nvim/lua/plugins/extras/util/snacks-extended.lua
+++ .config/nvim/lua/plugins/extras/util/snacks-extended.lua
@@ -60,9 +60,39 @@ return {
   },
   -- stylua: ignore
   keys = {
-    { "<leader>fz", function() Snacks.picker.zoxide() end, desc = "Zoxide" },
+    { "<leader>fz", function() Snacks.picker.zoxide({
+      finder = "files_zoxide",
+      format = "file",
+      -- confirm = "load_session" -- Disable loading session by default.
+      confirm = function(picker, item)
+        picker:close()
+        if item then
+          Snacks.picker.files({ cwd = item.text })
+        end
+        local dir = item.file
+        vim.fn.chdir(dir)
+      end,
+      win = {
+        preview = {
+          minimal = true,
+        },
+      },
+    }) end, desc = "Zoxide" },
     { "<leader>S", function() Snacks.picker.spelling() end, desc = "Spelling" },
-    { "<leader>B", function() Snacks.picker.buffers() end, desc = "Buffers" },
+    { "<leader>B", function() Snacks.picker.buffers({
+      on_show = function()
+        vim.cmd.stopinsert()
+      end,
+      current = false,
+      sort_lastused = true,
+    }) end, desc = "Buffers" },
+    { "<leader>,", function() Snacks.picker.buffers({
+      on_show = function()
+        vim.cmd.stopinsert()
+      end,
+      current = false,
+      sort_lastused = true,
+    }) end, desc = "Buffers" },
     { "<leader>gB", function() Snacks.picker.git_branches() end, desc = "Git Branches" },
     { "<leader>go", function() Snacks.gitbrowse() end, desc = "Git Open Line" },
   },
