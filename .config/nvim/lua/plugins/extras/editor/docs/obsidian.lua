144bbb54bd325a062610dac584f1c22939413c67
Sergio Laín <sergio.lac@pm.me>
2025-07-13 00:59:35 +0200

    fix(nvim): obsidian image uri

--- .config/nvim/lua/plugins/extras/editor/docs/obsidian.lua
+++ .config/nvim/lua/plugins/extras/editor/docs/obsidian.lua
@@ -114,6 +114,14 @@ return {
         img_folder = "00 - Data/Documentos",
       },
 
+      image = {
+        resolve = function(path, src)
+          if require("obsidian.api").path_is_note(path) then
+            return require("obsidian.api").resolve_image_path(src)
+          end
+        end,
+      },
+
       ui = { enable = false },
 
       statusline = {
