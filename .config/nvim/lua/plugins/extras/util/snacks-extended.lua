e3497e0bc1ba49e27b4cff2b9c932e74ff57b5d9
Sergio Laín <sergio.lac@pm.me>
2025-08-02 19:19:38 +0200

    feat(nvim): remove keys in the snacks dashboard

--- .config/nvim/lua/plugins/extras/util/snacks-extended.lua
+++ .config/nvim/lua/plugins/extras/util/snacks-extended.lua
@@ -3,6 +3,7 @@ return {
   opts = {
     dashboard = {
       preset = {
+        keys = {},
         header = [[
                                                                    
       ████ ██████           █████      ██                    
