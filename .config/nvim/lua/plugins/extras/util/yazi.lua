50ffd3a435dc029d1d0da377143355deb434a1f7
Sergio Laín <sergio.lac@pm.me>
2025-08-02 19:20:24 +0200

    feat(nvim): yazi and snacks integration

--- .config/nvim/lua/plugins/extras/util/yazi.lua
+++ .config/nvim/lua/plugins/extras/util/yazi.lua
@@ -9,7 +9,11 @@ return {
     opts = {
       open_for_directories = true,
       floating_window_scaling_factor = 0.8,
-      yazi_floating_window_border = "none",
+      yazi_floating_window_border = "rounded",
+      integrations = {
+        grep_in_selected_files = "snacks.picker",
+        grep_in_directory = "snacks.picker",
+      },
     },
   },
   {
