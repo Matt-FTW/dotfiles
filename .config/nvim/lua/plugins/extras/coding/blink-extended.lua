0269094a4e3688a8e0a31dda0e74f3ea8b549a40
Sergio Laín <sergio.lac@pm.me>
2025-08-02 19:20:06 +0200

    feat(nvim): blink bindings for the documentation

--- .config/nvim/lua/plugins/extras/coding/blink-extended.lua
+++ .config/nvim/lua/plugins/extras/coding/blink-extended.lua
@@ -4,8 +4,14 @@ return {
     keymap = {
       ["<C-k>"] = { "select_prev", "fallback" },
       ["<C-j>"] = { "select_next", "fallback" },
+      ["<C-u>"] = { "scroll_documentation_up", "fallback" },
+      ["<C-d>"] = { "scroll_documentation_down", "fallback" },
     },
     completion = {
+      menu = {
+        border = "rounded",
+        scrollbar = true,
+      },
       documentation = {
         window = {
           border = "rounded",
