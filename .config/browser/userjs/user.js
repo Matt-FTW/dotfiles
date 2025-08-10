ae952575fc858c47273aba6a22359a4ec8c3d3cd
Sergio Laín <sergio.lac@pm.me>
2025-08-10 19:03:33 +0200

    refactor(browser): only maintain userChrome and user.js files for zen

---
+++ .config/browser/userjs/user.js
@@ -0,0 +1,41 @@
+// THIS USER.JS IS INTENTED TO USE ONLY WITH ZEN BROWSER
+// Smooth Scrolling
+user_pref("apz.overscroll.enabled", true);
+user_pref("general.smoothScroll", true);
+user_pref("general.smoothScroll.msdPhysics.enabled", true);
+user_pref("mousewheel.default.delta_multiplier_y", 250);
+
+// Custom Options
+user_pref("ui.key.menuAccessKey", 0); // Disable alt key to allow custom bindings that uses it
+
+// Firefox Options
+user_pref("privacy.history.custom", true);
+user_pref("privacy.clearHistory.formdata", true);
+user_pref("browser.ctrlTab.sortByRecentlyUsed", true);
+user_pref("browser.download.useDownloadDir", false);
+user_pref("devtools.editor.keymap", "jim");
+user_pref("devtools.theme", "dark");
+user_pref("font.name.serif.x-western", "Maple Mono NF");
+user_pref("signon.rememberSignons", false);
+user_pref("signon.autofillForms", false);
+
+// Zen Options
+user_pref("browser.urlbar.trimURLs", true);
+user_pref("browser.tabs.hoverPreview.enabled", true);
+user_pref("toolkit.tabbox.switchByScrolling", false);
+user_pref("zen.urlbar.replace-newtab", true);
+user_pref("zen.tabs.vertical.right-side", true);
+user_pref("zen.workspaces.open-new-tab-if-last-unpinned-tab-is-closed", false);
+user_pref("zen.theme.gradient", true);
+user_pref("zen.view.experimental-rounded-view", true);
+user_pref("zen.view.experimental-force-window-controls-left", false); // Using the sidebar at the right
+user_pref("zen.widget.linux.transparency", true);
+user_pref("zen.pinned-tab-manager.restore-pinned-tabs-to-pinned-url", true);
+user_pref("zen.splitView.change-on-hover", true);
+user_pref("zen.tabs.show-newtab-under", false);
+user_pref("zen.tabs.show-newtab-button-top", false);
+user_pref("zen.tabs.show-newtab-vertical", false);
+user_pref("zen.urlbar.behavior", "float");
+user_pref("zen.view.compact.hide-toolbar", true);
+user_pref("zen.workspaces.force-container-workspace", true);
+user_pref("zen.glance.open-essential-external-links", false);
