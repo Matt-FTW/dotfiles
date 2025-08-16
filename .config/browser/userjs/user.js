a5f37c197054e7dd228aa4c3fee5077e8f79a9c9
Sergio Laín <sergio.lac@pm.me>
2025-08-17 01:36:26 +0200

    feat(browser): add some options to the user.js file

--- .config/browser/userjs/user.js
+++ .config/browser/userjs/user.js
@@ -18,6 +18,7 @@ user_pref("devtools.theme", "dark");
 user_pref("font.name.serif.x-western", "Maple Mono NF");
 user_pref("signon.rememberSignons", false);
 user_pref("signon.autofillForms", false);
+user_pref("browser.urlbar.suggest.searches", false);
 
 // Zen Options
 user_pref("browser.urlbar.trimURLs", true);
@@ -27,9 +28,9 @@ user_pref("zen.urlbar.replace-newtab", true);
 user_pref("zen.tabs.vertical.right-side", true);
 user_pref("zen.workspaces.open-new-tab-if-last-unpinned-tab-is-closed", false);
 user_pref("zen.theme.gradient", true);
-user_pref("zen.view.experimental-rounded-view", true);
+user_pref("zen.view.experimental-rounded-view", false);
 user_pref("zen.view.experimental-force-window-controls-left", false); // Using the sidebar at the right
-user_pref("zen.widget.linux.transparency", true);
+user_pref("zen.widget.linux.transparency", false);
 user_pref("zen.pinned-tab-manager.restore-pinned-tabs-to-pinned-url", true);
 user_pref("zen.splitView.change-on-hover", true);
 user_pref("zen.tabs.show-newtab-under", false);
@@ -39,3 +40,4 @@ user_pref("zen.urlbar.behavior", "float");
 user_pref("zen.view.compact.hide-toolbar", true);
 user_pref("zen.workspaces.force-container-workspace", true);
 user_pref("zen.glance.open-essential-external-links", false);
+user_pref("zen.theme.content-element-separation", 0);
