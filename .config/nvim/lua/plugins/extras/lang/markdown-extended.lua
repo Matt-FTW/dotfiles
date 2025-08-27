8bd4110a9f6521e21df22bbb2315751263b2aa94
Sergio Laín <sergio.lac@pm.me>
2025-08-27 13:22:23 +0200

    feat(nvim): remove plugins from python and markdown extended extras

--- .config/nvim/lua/plugins/extras/lang/markdown-extended.lua
+++ .config/nvim/lua/plugins/extras/lang/markdown-extended.lua
@@ -3,35 +3,6 @@ local filetype = { "markdown", "text", "tex", "plaintex", "norg" }
 return {
   { import = "lazyvim.plugins.extras.lang.markdown" },
   {
-    "gaoDean/autolist.nvim",
-    enabled = false,
-    ft = filetype,
-    opts = {},
-    keys = {
-      { "<CR>", "<CR><cmd>AutolistNewBullet<cr>", mode = { "i" }, ft = filetype },
-      { "o", "o<cmd>AutolistNewBullet<cr>", mode = { "n" }, ft = filetype },
-      { "O", "O<cmd>AutolistNewBulletBefore<cr>", mode = { "n" }, ft = filetype },
-      { "<CR>", "<cmd>AutolistToggleCheckbox<cr><CR>", mode = { "n" }, ft = filetype },
-      { "<C-r>", "<cmd>AutolistRecalculate<cr>", mode = { "n" }, ft = filetype },
-
-      { "].", "<cmd>AutolistCycleNext<cr>", mode = { "n" }, desc = "Next List Type", ft = filetype },
-      { "[.", "<cmd>AutolistCyclePrev<cr>", mode = { "n" }, desc = "Prev List Type", ft = filetype },
-
-      { ">>", ">><cmd>AutolistRecalculate<cr>", mode = { "n" }, ft = filetype },
-      { "<<", "<<<cmd>AutolistRecalculate<cr>", mode = { "n" }, ft = filetype },
-      { "dd", "dd<cmd>AutolistRecalculate<cr>", mode = { "n" }, ft = filetype },
-      { "d", "d<cmd>AutolistRecalculate<cr>", mode = { "v" }, ft = filetype },
-    },
-  },
-  {
-    "antonk52/markdowny.nvim",
-    enabled = false,
-    ft = filetype,
-    opts = {
-      filetypes = filetype,
-    },
-  },
-  {
     "mfussenegger/nvim-lint",
     opts = {
       linters = {
--- .config/nvim/lua/plugins/extras/lang/python-extended.lua
+++ .config/nvim/lua/plugins/extras/lang/python-extended.lua
@@ -1,5 +1,3 @@
-local package_prefix = "<leader>pp"
-
 -- LSP Server to use for Python.
 -- Set to "basedpyright" to use basedpyright instead of pyright.
 vim.g.lazyvim_python_lsp = "basedpyright"
@@ -12,32 +10,6 @@ return {
     opts = {
       ensure_installed = {
         "requirements",
-      },
-    },
-  },
-  {
-    "MeanderingProgrammer/py-requirements.nvim",
-    enabled = false,
-    event = {
-      "BufRead requirements.txt",
-    },
-    dependencies = {
-      { "nvim-lua/plenary.nvim" },
-    },
-    opts = {},
-    -- stylua: ignore
-    keys = {
-      { package_prefix .. "u", function() require("py-requirements").upgrade() end, desc = "Update Package" },
-      { package_prefix .. "i", function() require("py-requirements").show_description() end, desc = "Package Info" },
-      { package_prefix .. "a", function() require("py-requirements").upgrade_all() end, desc = "Update All Packages" },
-    },
-  },
-  {
-    "folke/which-key.nvim",
-    opts = {
-      spec = {
-        { "<leader>p", group = "packages/dependencies", icon = " " },
-        { package_prefix, group = "python", icon = " " },
       },
     },
   },
