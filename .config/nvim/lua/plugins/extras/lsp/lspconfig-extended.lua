ed28377130b9fb9af6540c4431fb6babd166374c
Sergio Laín <sergio.lac@pm.me>
2025-11-29 01:15:38 +0100

    fix(nvim): lspconfig keymaps definition
    LazyVim changed the way the keymaps are defined.

--- .config/nvim/lua/plugins/extras/lsp/lspconfig-extended.lua
+++ .config/nvim/lua/plugins/extras/lsp/lspconfig-extended.lua
@@ -1,43 +1,28 @@
-local nvim_0_10 = vim.fn.has("nvim-0.10")
 local prefix = "<leader>cl"
 
 return {
   {
     "neovim/nvim-lspconfig",
-    init = function()
-      local keys = require("lazyvim.plugins.lsp.keymaps").get()
-
-      keys[#keys + 1] = { prefix, false }
-      keys[#keys + 1] = { "<leader>cil", "<cmd>LspInfo<cr>", desc = "Lsp" }
-      keys[#keys + 1] = { prefix .. "r", "<cmd>LspRestart<cr>", desc = "Restart Lsp" }
-      keys[#keys + 1] = { prefix .. "s", "<cmd>LspStart<cr>", desc = "Start Lsp" }
-      keys[#keys + 1] = { prefix .. "S", "<cmd>LspStop<cr>", desc = "Stop Lsp" }
-      keys[#keys + 1] = { "E", vim.diagnostic.open_float, desc = "Line Diagnostics" }
-
-      -- stylua: ignore start
-      keys[#keys + 1] = { prefix .. "W", function() vim.lsp.buf.remove_workspace_folder() end, desc = "Remove workspace" }
-      keys[#keys + 1] = { prefix .. "w", function() vim.lsp.buf.add_workspace_folder() end, desc = "Add workspace" }
-      -- stylua: ignore end
-    end,
     opts = {
       diagnostics = {
         virtual_text = false,
-      },
-      inlay_hints = {
-        enabled = nvim_0_10,
       },
       codelens = {
         enabled = false,
       },
       servers = {
-        lua_ls = {
-          settings = {
-            Lua = {
-              hint = {
-                enable = nvim_0_10,
-                setType = nvim_0_10,
-              },
-            },
+        ["*"] = {
+          keys = {
+            { prefix, false },
+            { "<leader>cil", "<cmd>LspInfo<cr>", desc = "Lsp" },
+            { prefix .. "r", "<cmd>LspRestart<cr>", desc = "Restart Lsp" },
+            { prefix .. "s", "<cmd>LspStart<cr>", desc = "Start Lsp" },
+            { prefix .. "S", "<cmd>LspStop<cr>", desc = "Stop Lsp" },
+            { "E", vim.diagnostic.open_float, desc = "Line Diagnostics" },
+            -- stylua: ignore start
+            { prefix .. "W", function() vim.lsp.buf.remove_workspace_folder() end, desc = "Remove workspace" },
+            { prefix .. "w", function() vim.lsp.buf.add_workspace_folder() end, desc = "Add workspace" },
+            -- stylua: ignore end
           },
         },
       },
