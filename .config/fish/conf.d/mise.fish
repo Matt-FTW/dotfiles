723223d3096a63b1858f416255aa9703c742e1d7
Sergio Laín <sergio.lac@pm.me>
2025-12-07 01:45:18 +0100

    feat(fish): activate shims for mise if in interactive mode

--- .config/fish/conf.d/mise.fish
+++ .config/fish/conf.d/mise.fish
@@ -1,3 +1,7 @@
 if type -q mise
-    mise activate fish | source
+    if status is-interactive
+        mise activate fish | source
+    else
+        mise activate fish --shims | source
+    end
 end
