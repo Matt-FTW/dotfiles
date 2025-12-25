c7acc73b901748250d56811c34fb077e52e46f22
Sergio Laín <sergio.lac@pm.me>
2025-12-25 01:43:24 +0100

    feat(nvim): change default python lsp to ty

--- .config/nvim/lua/plugins/extras/lang/python-extended.lua
+++ .config/nvim/lua/plugins/extras/lang/python-extended.lua
@@ -1,6 +1,6 @@
 -- LSP Server to use for Python.
 -- Set to "basedpyright" to use basedpyright instead of pyright.
-vim.g.lazyvim_python_lsp = "basedpyright"
+vim.g.lazyvim_python_lsp = "ty"
 vim.g.lazyvim_python_ruff = "ruff"
 
 return {
@@ -10,6 +10,14 @@ return {
     opts = {
       ensure_installed = {
         "requirements",
+      },
+    },
+  },
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "ty",
       },
     },
   },
