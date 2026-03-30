e5ae871d01d98b8ae14398c90f60547358befd85
Sergio Laín <sergio.lac@pm.me>
2026-03-30 20:30:35 +0200

    fix(nvim): change catppuccin colorscheme name

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -13,7 +13,7 @@ require("lazy").setup({
       "LazyVim/LazyVim",
       import = "lazyvim.plugins",
       opts = {
-        colorscheme = "catppuccin",
+        colorscheme = "catppuccin-nvim",
       },
     },
     {
